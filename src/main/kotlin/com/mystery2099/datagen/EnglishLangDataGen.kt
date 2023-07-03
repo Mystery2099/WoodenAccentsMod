2964fa0e306bb7055f1505c422b62c1bc9e416c8
Mystery2099 <xxmysteryxxftb@gmail.com>
2023-07-03 18:10:52 -0400

    created method for generating lang names for item groups.

--- src/main/kotlin/com/mystery2099/datagen/EnglishLangDataGen.kt
+++ src/main/kotlin/com/mystery2099/datagen/EnglishLangDataGen.kt
@@ -3,12 +3,19 @@ package com.mystery2099.datagen
 import com.mystery2099.WoodenAccentsModItemGroups
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricLanguageProvider
+import net.minecraft.item.ItemGroup
 
 class EnglishLangDataGen(dataOutput: FabricDataOutput) : FabricLanguageProvider(dataOutput) {
+    private var builder: TranslationBuilder? = null
     override fun generateTranslations(translationBuilder: TranslationBuilder) {
-        WoodenAccentsModItemGroups.itemGroups.forEach { group ->
-            translationBuilder.add(group, "Wooden Accents Mod: ${group.id.path.toName()}")
-        }
+        builder = translationBuilder
+
+        WoodenAccentsModItemGroups.itemGroups.forEach(this::genLangName)
+
+    }
+
+    private fun genLangName(itemGroup: ItemGroup) {
+        builder?.add(itemGroup, "Wooden Accents Mod: ${itemGroup.id.path.toName()}")
     }
 
     private fun String.toName(): String {
