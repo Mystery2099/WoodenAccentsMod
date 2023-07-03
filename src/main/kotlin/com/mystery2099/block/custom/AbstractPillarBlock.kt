2d2b17b969a8e3b794d1ade3c8656c73a175e147
Mystery2099 <xxmysteryxxftb@gmail.com>
2023-07-03 19:28:24 -0400

    Added full functionality to ThinPillarBlock(Hopefully)

--- src/main/kotlin/com/mystery2099/block/custom/AbstractPillarBlock.kt
+++ src/main/kotlin/com/mystery2099/block/custom/AbstractPillarBlock.kt
@@ -86,17 +86,8 @@ abstract class AbstractPillarBlock(baseBlock: Block, size: Size) : AbstractWater
         return this.getStateAtPos(pos.down())
     }
 
-    open fun WorldAccess.checkUp(pos: BlockPos): Boolean {
-        val here = this.getBlockState(pos)
-        val up = this.getUpState(pos)
-        return up.block is ThinPillarBlock && !here.get(connectionLocked) && !up.get(connectionLocked)
-    }
-
-    open fun WorldAccess.checkDown(pos: BlockPos): Boolean {
-        val here = this.getBlockState(pos)
-        val down = this.getDownState(pos)
-        return down.block is ThinPillarBlock && !here.get(connectionLocked) && !down.get(connectionLocked)
-    }
+    abstract fun WorldAccess.checkUp(pos: BlockPos): Boolean
+    abstract fun WorldAccess.checkDown(pos: BlockPos): Boolean
 
 
 
---
+++ src/main/kotlin/com/mystery2099/block/custom/ThinPillarBlock.kt
@@ -0,0 +1,33 @@
+package com.mystery2099.block.custom
+
+import net.minecraft.block.Block
+import net.minecraft.util.math.BlockPos
+import net.minecraft.world.WorldAccess
+
+class ThinPillarBlock(baseBlock: Block) : AbstractPillarBlock(baseBlock, size) {
+
+    override fun WorldAccess.checkUp(pos: BlockPos): Boolean {
+        val here = this.getBlockState(pos)
+        val up = this.getUpState(pos)
+        return up.block is ThinPillarBlock && !here.get(connectionLocked) && !up.get(connectionLocked)
+    }
+
+    override fun WorldAccess.checkDown(pos: BlockPos): Boolean {
+        val here = this.getBlockState(pos)
+        val down = this.getDownState(pos)
+        return down.block is ThinPillarBlock && !here.get(connectionLocked) && !down.get(connectionLocked)
+    }
+    companion object {
+        @JvmStatic
+        val size = Size(
+            createCuboidShape(4.0, 13.0, 4.0, 12.0, 16.0, 12.0),
+            createCuboidShape(6.0, 1.0, 6.0, 10.0, 16.0, 10.0),
+            createCuboidShape(4.0, 0.0, 4.0, 12.0, 3.0, 12.0)
+        )
+        @JvmStatic
+        val instances = HashSet<ThinPillarBlock>()
+    }
+    init {
+        instances.add(this)
+    }
+}
