ec2ea50ce22fffec5484520b505d081897443fc0
Mystery2099 <xxmysteryxxftb@gmail.com>
2023-07-03 19:33:21 -0400

    Added register method for registering blocks

--- src/main/kotlin/com/mystery2099/block/ModBlocks.kt
+++ src/main/kotlin/com/mystery2099/block/ModBlocks.kt
@@ -1,10 +1,20 @@
 package com.mystery2099.block
 
 import com.mystery2099.WoodenAccentsMod
+import net.minecraft.block.Block
+import net.minecraft.registry.Registries
+import net.minecraft.registry.Registry
+import net.minecraft.util.Identifier
 
 object ModBlocks {
 
 
+
+
+    private fun Block.register(identifier: Identifier): Block {
+        return Registry.register(Registries.BLOCK, identifier, this)
+
+    }
     fun register() {
         WoodenAccentsMod.logger.info("Registering ModBlocks for mod: ${WoodenAccentsMod.modid}")
     }
