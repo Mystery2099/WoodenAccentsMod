ebfc604bfb72063bf155437887acbfacb7e0a391
Mystery2099 <xxmysteryxxftb@gmail.com>
2023-07-03 08:48:42 -0400

    Added item groups to lang & created extension method for creating names from strings

--- src/main/kotlin/com/mystery2099/datagen/EnglishLangDataGen.kt
+++ src/main/kotlin/com/mystery2099/datagen/EnglishLangDataGen.kt
@@ -1,9 +1,22 @@
 package com.mystery2099.datagen
 
+import com.mystery2099.WoodenAccentsModItemGroups
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricLanguageProvider
 
 class EnglishLangDataGen(dataOutput: FabricDataOutput) : FabricLanguageProvider(dataOutput) {
-    override fun generateTranslations(translationBuilder: TranslationBuilder?) {
+    override fun generateTranslations(translationBuilder: TranslationBuilder) {
+        WoodenAccentsModItemGroups.itemGroups.forEach { group ->
+            translationBuilder.add(group, "Wooden Accents Mod: ${group.id.path.toName()}")
+        }
+    }
+
+    private fun String.toName(): String {
+        val parts = this.split("_")
+        var output = ""
+        parts.forEach { str ->
+            output += str.replaceFirstChar { it.uppercase() }.replace("_", " ") + " "
+        }
+        return output.removeSuffix(" ")
     }
 }
