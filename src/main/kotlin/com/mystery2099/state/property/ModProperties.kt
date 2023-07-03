ed80da692229ddb799b98c99fddd2d2b68b72c0b
Mystery2099 <xxmysteryxxftb@gmail.com>
2023-07-03 18:58:28 -0400

    Added connectionLocked property

---
+++ src/main/kotlin/com/mystery2099/state/property/ModProperties.kt
@@ -0,0 +1,9 @@
+package com.mystery2099.state.property
+
+import net.minecraft.state.property.BooleanProperty
+
+object ModProperties {
+    @JvmStatic
+    val connectionLocked: BooleanProperty = BooleanProperty.of("connection_locked")!!
+
+}
