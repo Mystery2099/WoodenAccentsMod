d033c2f841d01d13127f14b72378827c83bf2a64
Mystery2099 <xxmysteryxxftb@gmail.com>
2023-07-04 17:58:24 -0400

    Added @JvmStatic to variables

--- src/main/kotlin/com/mystery2099/data/ModModels.kt
+++ src/main/kotlin/com/mystery2099/data/ModModels.kt
@@ -7,65 +7,85 @@ import java.util.*
 
 object ModModels {
     /*---------------Texture Keys----------------*/
-    val legs = TextureKey.of("legs")
+    @JvmStatic
+    val legs: TextureKey = TextureKey.of("legs")
 
 
     /*---------------Outside Stuff----------------*/
 
     //Thin Pillar
+    @JvmStatic
     val thinPillarInventory = block("thin_pillar", "_inventory", TextureKey.ALL)
+    @JvmStatic
     val thinPillarTop = block("thin_pillar_top", "_top", TextureKey.ALL)
+    @JvmStatic
     val thinPillarBottom = block("thin_pillar_base", "_base", TextureKey.ALL)
+    @JvmStatic
     val thinPillarCenter = block("thin_pillar_center", "_center", TextureKey.ALL)
     //Thick Pillar
+    @JvmStatic
     val thickPillarInventory = block("thick_pillar", "_inventory", TextureKey.ALL)
+    @JvmStatic
     val thickPillarTop = block("thick_pillar_top", "_top", TextureKey.ALL)
+    @JvmStatic
     val thickPillarBottom = block("thick_pillar_base", "_base", TextureKey.ALL)
+    @JvmStatic
     val thickPillarCenter = block("thick_pillar_center", "_center", TextureKey.ALL)
 
     /*---------------Living Room Stuff----------------*/
     //Coffee Table Models
-    val COFFEE_TABLE: Model = block("coffee_table", TextureKey.TOP, legs)
-    val TALL_COFFEE_TABLE_TOP: Model = block("coffee_table_top_tall",
+    @JvmStatic
+    val coffeeTableInventory: Model = block("coffee_table", "_inventory", TextureKey.TOP, legs)
+    @JvmStatic
+    val coffeeTableTopTall: Model = block("coffee_table_top_tall",
         "_top_tall", TextureKey.TOP)
-    val TALL_COFFEE_TABLE_NORTH_EAST_LEG: Model = block(
+    @JvmStatic
+    val coffeeTableNorthEastLegTall: Model = block(
         "coffee_table_north_east_leg_tall",
         "_north_east_leg_tall",
         legs
     )
-    val TALL_COFFEE_TABLE_NORTH_WEST_LEG: Model = block(
+    @JvmStatic
+    val coffeeTableNorthWestLegTall: Model = block(
         "coffee_table_north_west_leg_tall",
         "_north_west_leg_tall",
         legs
     )
-    val TALL_COFFEE_TABLE_SOUTH_EAST_LEG: Model = block(
+    @JvmStatic
+    val coffeeTableSouthEastLegTall: Model = block(
         "coffee_table_south_east_leg_tall",
         "_south_east_leg_tall",
         legs
     )
-    val TALL_COFFEE_TABLE_SOUTH_WEST_LEG: Model = block(
+    @JvmStatic
+    val coffeeTableSouthWestLegTall: Model = block(
         "coffee_table_south_west_leg_tall",
         "_south_west_leg_tall",
         legs
     )
-    val SHORT_COFFEE_TABLE_TOP: Model =
+    @JvmStatic
+    val coffeeTableTopShort: Model =
         block("coffee_table_top_short", "_top_short", TextureKey.TOP)
-    val SHORT_COFFEE_TABLE_NORTH_EAST_LEG: Model = block(
+    @JvmStatic
+    val coffeeTableNorthEastLegShort: Model = block(
         "coffee_table_north_east_leg_short",
         "_north_east_leg_short",
         legs
     )
-    val SHORT_COFFEE_TABLE_NORTH_WEST_LEG: Model = block(
+    @JvmStatic
+    val coffeeTableNorthWestLegShort: Model = block(
         "coffee_table_north_west_leg_short",
         "_north_west_leg_short",
         legs
     )
-    val SHORT_COFFEE_TABLE_SOUTH_EAST_LEG: Model = block(
+    @JvmStatic
+    val coffeeTableSouthEastLegShort: Model = block(
         "coffee_table_south_east_leg_short",
         "_south_east_leg_short",
         legs
     )
-    val SHORT_COFFEE_TABLE_SOUTH_WEST_LEG: Model = block(
+    @JvmStatic
+    val coffeeTableSouthWestLegShort: Model = block(
         "coffee_table_south_west_leg_short",
         "_south_west_leg_short",
         legs
