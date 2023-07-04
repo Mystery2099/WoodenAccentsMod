50a9860623d2b7c830b124308cc865f00b6c7ee3
Mystery2099 <xxmysteryxxftb@gmail.com>
2023-07-04 19:28:03 -0400

    created addDropsDoubleWithProperty method which makes it easy to add loot tables similar to the slab. Used addDropsDoubleWithProperty with every instance of CoffeeTableBlock via a forEach loop

--- src/main/kotlin/com/mystery2099/datagen/BlockLootTableDataGen.kt
+++ src/main/kotlin/com/mystery2099/datagen/BlockLootTableDataGen.kt
@@ -1,12 +1,27 @@
 package com.mystery2099.datagen
 
+import com.mystery2099.block.custom.CoffeeTableBlock
+import com.mystery2099.block.custom.enums.CoffeeTableType
+import com.mystery2099.state.property.ModProperties
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricBlockLootTableProvider
 import net.minecraft.block.Block
+import net.minecraft.loot.LootPool
+import net.minecraft.loot.LootTable
+import net.minecraft.loot.condition.BlockStatePropertyLootCondition
+import net.minecraft.loot.entry.ItemEntry
+import net.minecraft.loot.function.SetCountLootFunction
+import net.minecraft.loot.provider.number.ConstantLootNumberProvider
+import net.minecraft.predicate.StatePredicate
+import net.minecraft.state.property.Property
+import net.minecraft.util.StringIdentifiable
 
 class BlockLootTableDataGen(dataOutput: FabricDataOutput) : FabricBlockLootTableProvider(dataOutput) {
     override fun generate() {
         dropsSelf.forEach(::addDrop)
+        CoffeeTableBlock.instances.forEach{
+            addDropsDoubleWithProperty(it, ModProperties.coffeeTableType, CoffeeTableType.TALL)
+        }
     }
 
     companion object {
@@ -16,4 +31,22 @@ class BlockLootTableDataGen(dataOutput: FabricDataOutput) : FabricBlockLootTable
         }
     }
 
+    private fun <T> addDropsDoubleWithProperty(
+        drop: Block,
+        property: Property<T>,
+        value: T
+    ) where T : Comparable<T>, T : StringIdentifiable {
+        val builder = LootTable.builder().pool(
+            LootPool.builder().rolls(ConstantLootNumberProvider.create(1.0f)).with(
+                applyExplosionDecay(
+                    drop, ItemEntry.builder(drop).apply(SetCountLootFunction.builder(ConstantLootNumberProvider.create(2.0f))
+                        .conditionally(BlockStatePropertyLootCondition.builder(drop)
+                            .properties(StatePredicate.Builder.create().exactMatch(property, value))
+                        )
+                    )
+                )
+            )
+        )
+        addDrop(drop, builder)
+    }
 }
