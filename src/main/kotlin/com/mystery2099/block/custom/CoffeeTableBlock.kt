b3c6622480b7e9b08a9d69171b941d18f17f4e73
Mystery2099 <xxmysteryxxftb@gmail.com>
2023-07-04 17:00:45 -0400

    Added methods for checking north, east, south, and west for another coffee table block

--- src/main/kotlin/com/mystery2099/block/custom/CoffeeTableBlock.kt
+++ src/main/kotlin/com/mystery2099/block/custom/CoffeeTableBlock.kt
@@ -4,14 +4,13 @@ import com.mystery2099.block.custom.enums.CoffeeTableType
 import com.mystery2099.state.property.ModProperties
 import net.fabricmc.fabric.api.`object`.builder.v1.block.FabricBlockSettings
 import net.minecraft.block.Block
+import net.minecraft.state.property.BooleanProperty
 import net.minecraft.state.property.Properties
+import net.minecraft.util.math.BlockPos
+import net.minecraft.util.math.Direction
+import net.minecraft.world.WorldAccess
 
-class CoffeeTableBlock(private val _legBlock: Block, private val _topBlock: Block) : AbstractWaterloggableBlock(FabricBlockSettings.copyOf(_topBlock)) {
-    val legBlock: Block
-        get() = _legBlock
-    val topBlock: Block
-        get() = _topBlock
-
+class CoffeeTableBlock(val legBlock: Block, val topBlock: Block) : AbstractWaterloggableBlock(FabricBlockSettings.copyOf(topBlock)) {
     init {
         defaultState = defaultState.with(north, false)
             .with(east, false).with(south, false)
@@ -19,17 +18,43 @@ class CoffeeTableBlock(private val _legBlock: Block, private val _topBlock: Bloc
         instances.add(this)
     }
 
+
+
+    private fun WorldAccess.checkDirection(pos: BlockPos, direction: Direction): Boolean {
+        val here = this.getBlockState(pos)
+        val there = this.getBlockState(pos.offset(direction))
+        return there.block is CoffeeTableBlock && here.get(type) == there.get(type)
+    }
+
+    private fun WorldAccess.checkNorth(pos: BlockPos): Boolean {
+        return this.checkDirection(pos, Direction.NORTH)
+    }
+
+    private fun WorldAccess.checkEast(pos: BlockPos): Boolean {
+        return this.checkDirection(pos, Direction.EAST)
+    }
+
+    private fun WorldAccess.checkSouth(pos: BlockPos): Boolean {
+        return this.checkDirection(pos, Direction.SOUTH)
+    }
+
+    private fun WorldAccess.checkWest(pos: BlockPos): Boolean {
+        return this.checkDirection(pos, Direction.WEST)
+    }
+
+
+
     companion object {
         @JvmStatic
         val instances = HashSet<CoffeeTableBlock>()
         @JvmStatic
-        val north = Properties.NORTH
+        val north: BooleanProperty = Properties.NORTH
         @JvmStatic
-        val east = Properties.EAST
+        val east: BooleanProperty = Properties.EAST
         @JvmStatic
-        val south = Properties.SOUTH
+        val south: BooleanProperty = Properties.SOUTH
         @JvmStatic
-        val west = Properties.WEST
+        val west: BooleanProperty = Properties.WEST
         @JvmStatic
         val type = ModProperties.coffeeTableType
     }
