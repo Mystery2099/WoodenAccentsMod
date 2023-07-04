cc818cc8b7c65a6a2516720becb711a2408d345a
Mystery2099 <xxmysteryxxftb@gmail.com>
2023-07-03 20:03:11 -0400

    Added method for adding collections to a tag and added axeMineable HashSet for any Blocks which are mined with an axe

--- src/main/kotlin/com/mystery2099/datagen/BlockTagDataGen.kt
+++ src/main/kotlin/com/mystery2099/datagen/BlockTagDataGen.kt
@@ -1,15 +1,31 @@
 package com.mystery2099.datagen
 
+import com.mystery2099.block.custom.ThinPillarBlock
 import com.mystery2099.data.ModBlockTags
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider
+import net.minecraft.block.Block
 import net.minecraft.block.Blocks
 import net.minecraft.registry.RegistryWrapper
+import net.minecraft.registry.tag.BlockTags
+import net.minecraft.registry.tag.TagKey
+import java.util.HashSet
 import java.util.concurrent.CompletableFuture
 
 class BlockTagDataGen( output : FabricDataOutput,  registriesFuture : CompletableFuture<RegistryWrapper.WrapperLookup>) : FabricTagProvider.BlockTagProvider(output, registriesFuture) {
+
     override fun configure(arg: RegistryWrapper.WrapperLookup) {
         getOrCreateTagBuilder(ModBlockTags.TEST_TAG).add(Blocks.ACACIA_BUTTON)
-
+        BlockTags.AXE_MINEABLE.addCollection(axeMineable)
     }
+
+    companion object {
+        @JvmStatic
+        val axeMineable = HashSet<Block>()
+    }
+
+    private fun <T : Block> TagKey<Block>.addCollection(collection: Collection<T>) {
+        collection.forEach(getOrCreateTagBuilder(this)::add)
+    }
+
 }
