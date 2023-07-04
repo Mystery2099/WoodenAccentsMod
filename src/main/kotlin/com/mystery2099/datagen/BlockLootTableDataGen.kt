f1a1148b213b221f4021e22f16c49d2597cbd46d
Mystery2099 <xxmysteryxxftb@gmail.com>
2023-07-04 18:58:23 -0400

    created dropsSelf Hasset and Block.dropsSelf() extension method

--- src/main/kotlin/com/mystery2099/datagen/BlockLootTableDataGen.kt
+++ src/main/kotlin/com/mystery2099/datagen/BlockLootTableDataGen.kt
@@ -2,8 +2,18 @@ package com.mystery2099.datagen
 
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricBlockLootTableProvider
+import net.minecraft.block.Block
 
 class BlockLootTableDataGen(dataOutput: FabricDataOutput) : FabricBlockLootTableProvider(dataOutput) {
     override fun generate() {
+        dropsSelf.forEach(::addDrop)
     }
+
+    companion object {
+        val dropsSelf = HashSet<Block>()
+        fun Block.dropsSelf() {
+            dropsSelf.add(this)
+        }
+    }
+
 }
