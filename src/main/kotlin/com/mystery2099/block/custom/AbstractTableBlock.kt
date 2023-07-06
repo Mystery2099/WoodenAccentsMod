6799ee5bc6da22239cfc6c23ef5760fc8cbd8d5d
Mystery2099 <xxmysteryxxftb@gmail.com>
2023-07-05 23:03:09 -0400

    fixed states not working properly and missing north in default state

--- src/main/kotlin/com/mystery2099/block/custom/AbstractTableBlock.kt
+++ src/main/kotlin/com/mystery2099/block/custom/AbstractTableBlock.kt
@@ -1,6 +1,5 @@
 package com.mystery2099.block.custom
 
-import com.mystery2099.block.custom.enums.CoffeeTableType
 import net.fabricmc.fabric.api.`object`.builder.v1.block.FabricBlockSettings
 import net.minecraft.block.Block
 import net.minecraft.block.BlockState
@@ -13,11 +12,10 @@ import net.minecraft.world.WorldAccess
 
 abstract class AbstractTableBlock(baseBlock: Block, topBlock: Block) : AbstractWaterloggableBlock(FabricBlockSettings.copyOf(baseBlock)) {
     init {
-        defaultState = defaultState.apply {
-            with(east, false)
-            with(south, false)
-            with(west, false)
-        }
+        defaultState = defaultState.with(NORTH, false)
+            .with(EAST, false)
+            .with(SOUTH, false)
+            .with(WEST, false)
     }
 
     @Deprecated("Deprecated in Java")
@@ -29,44 +27,44 @@ abstract class AbstractTableBlock(baseBlock: Block, topBlock: Block) : AbstractW
         pos: BlockPos?,
         neighborPos: BlockPos?
     ): BlockState? {
-        return super.getStateForNeighborUpdate(state, direction, neighborState, world, pos!!, neighborPos)?.apply {
-            withIfExists(north, world.checkNorth(pos))
-            withIfExists(east, world.checkEast(pos))
-            withIfExists(south, world.checkSouth(pos))
-            withIfExists(west, world.checkWest(pos))
-        }
+        return super.getStateForNeighborUpdate(state, direction, neighborState, world, pos!!, neighborPos)
+            ?.withIfExists(NORTH, world.checkNorth(pos))
+            ?.withIfExists(EAST, world.checkEast(pos))
+            ?.withIfExists(SOUTH, world.checkSouth(pos))
+            ?.withIfExists(WEST, world.checkWest(pos))
+
     }
 
     override fun appendProperties(builder: StateManager.Builder<Block, BlockState>) {
         super.appendProperties(builder)
-        builder.add(north, east, south, west)
+        builder.add(NORTH, EAST, SOUTH, WEST)
     }
     open fun WorldAccess.checkDirection(pos: BlockPos, direction: Direction): Boolean {
-        return this.getBlockState(pos.offset(direction)).block is AbstractTableBlock
+        return getBlockState(pos.offset(direction)).block is AbstractTableBlock
     }
-    private fun WorldAccess.checkNorth(pos: BlockPos): Boolean {
-        return this.checkDirection(pos, Direction.NORTH)
-    }
-
-    private fun WorldAccess.checkEast(pos: BlockPos): Boolean {
-        return this.checkDirection(pos, Direction.EAST)
+    open fun WorldAccess.checkNorth(pos: BlockPos): Boolean {
+        return checkDirection(pos, Direction.NORTH)
     }
 
-    private fun WorldAccess.checkSouth(pos: BlockPos): Boolean {
-        return this.checkDirection(pos, Direction.SOUTH)
+    open fun WorldAccess.checkEast(pos: BlockPos): Boolean {
+        return checkDirection(pos, Direction.EAST)
     }
 
-    private fun WorldAccess.checkWest(pos: BlockPos): Boolean {
-        return this.checkDirection(pos, Direction.WEST)
+    open fun WorldAccess.checkSouth(pos: BlockPos): Boolean {
+        return checkDirection(pos, Direction.SOUTH)
+    }
+
+    open fun WorldAccess.checkWest(pos: BlockPos): Boolean {
+        return checkDirection(pos, Direction.WEST)
     }
     companion object {
         @JvmStatic
-        val north: BooleanProperty = Properties.NORTH
+        val NORTH: BooleanProperty = Properties.NORTH
         @JvmStatic
-        val east: BooleanProperty = Properties.EAST
+        val EAST: BooleanProperty = Properties.EAST
         @JvmStatic
-        val south: BooleanProperty = Properties.SOUTH
+        val SOUTH: BooleanProperty = Properties.SOUTH
         @JvmStatic
-        val west: BooleanProperty = Properties.WEST
+        val WEST: BooleanProperty = Properties.WEST
     }
 }
