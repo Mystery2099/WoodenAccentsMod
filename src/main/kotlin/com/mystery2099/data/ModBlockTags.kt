a2d178a50644dc2bd73c14d2031375df4a619f19
Mystery2099 <xxmysteryxxftb@gmail.com>
2023-07-06 22:48:19 -0400

    created map for block tags with matching item tags. added extension method to easliy add tags to it. created pillar, thick pillar, and thin pillar tags

--- src/main/kotlin/com/mystery2099/data/ModBlockTags.kt
+++ src/main/kotlin/com/mystery2099/data/ModBlockTags.kt
@@ -2,17 +2,32 @@ package com.mystery2099.data
 
 import com.mystery2099.WoodenAccentsMod
 import net.minecraft.block.Block
+import net.minecraft.item.Item
 import net.minecraft.registry.RegistryKeys
 import net.minecraft.registry.tag.TagKey
 import net.minecraft.util.Identifier
 
 object ModBlockTags {
-    val TEST_TAG = "test_tag".toBlockTag()
+    internal var blockTagWithMatchingItemTag: MutableMap<TagKey<Block>, TagKey<Item>> = HashMap()
+
+    val TEST_TAG: TagKey<Block> = "test_tag".toBlockTag().withMatchingItemTag()
+
+    /*---------------Outside Tags----------------*/
+    val pillars = "pillars".toBlockTag().withMatchingItemTag()
+    val thinPillars = "thin_pillars".toBlockTag().withMatchingItemTag()
+    val thickPillars = "thick_pillars".toBlockTag().withMatchingItemTag()
+    /*---------------End Outside Tags----------------*/
+
 
     private fun String.toBlockTag(namespace: String): TagKey<Block> {
         return TagKey.of(RegistryKeys.BLOCK, Identifier(namespace, this))
     }
-    fun String.toBlockTag(): TagKey<Block> {
+    private fun String.toBlockTag(): TagKey<Block> {
         return this.toBlockTag(WoodenAccentsMod.modid)
     }
+    private fun TagKey<Block>.withMatchingItemTag(): TagKey<Block> {
+        blockTagWithMatchingItemTag[this] = TagKey.of(RegistryKeys.ITEM, this.id)
+        return this
+    }
+
 }
