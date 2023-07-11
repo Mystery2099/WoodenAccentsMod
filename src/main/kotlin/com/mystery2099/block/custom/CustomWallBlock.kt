69ef66d24480d2aef71b406267aef5f8edb515bb
Mystery2099 <xxmysteryxxftb@gmail.com>
2023-07-11 18:44:15 -0400

    Added custom wall blocks to outside items tab

--- src/main/kotlin/com/mystery2099/block/custom/CustomWallBlock.kt
+++ src/main/kotlin/com/mystery2099/block/custom/CustomWallBlock.kt
@@ -1,10 +1,12 @@
 package com.mystery2099.block.custom
 
+import com.mystery2099.WoodenAccentsModItemGroups
 import net.fabricmc.fabric.api.`object`.builder.v1.block.FabricBlockSettings
 import net.minecraft.block.Block
 import net.minecraft.block.WallBlock
 
 class CustomWallBlock(val baseBlock: Block) : WallBlock(FabricBlockSettings.copyOf(baseBlock)) {
-
-
+    init {
+        WoodenAccentsModItemGroups.outsideItems += this
+    }
 }
