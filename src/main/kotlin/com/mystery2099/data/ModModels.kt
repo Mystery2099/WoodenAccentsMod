84269cc645e3c0fc7edbd620c5f21669cda25b24
Mystery2099 <xxmysteryxxftb@gmail.com>
2023-07-12 19:23:40 -0400

    Added plank ladder block model and its parent

--- src/main/kotlin/com/mystery2099/data/ModModels.kt
+++ src/main/kotlin/com/mystery2099/data/ModModels.kt
@@ -7,60 +7,43 @@ import java.util.*
 
 object ModModels {
     /*---------------Texture Keys----------------*/
-    @JvmStatic
     val legs: TextureKey = TextureKey.of("legs")
 
 
     /*---------------Outside Stuff----------------*/
 
     //Thin Pillar
-    @JvmStatic
     val thinPillarInventory = block("thin_pillar", "_inventory", TextureKey.ALL)
-    @JvmStatic
     val thinPillarTop = block("thin_pillar_top", "_top", TextureKey.ALL)
-    @JvmStatic
     val thinPillarBottom = block("thin_pillar_base", "_base", TextureKey.ALL)
-    @JvmStatic
     val thinPillarCenter = block("thin_pillar_center", "_center", TextureKey.ALL)
     //Thick Pillar
-    @JvmStatic
     val thickPillarInventory = block("thick_pillar", "_inventory", TextureKey.ALL)
-    @JvmStatic
     val thickPillarTop = block("thick_pillar_top", "_top", TextureKey.ALL)
-    @JvmStatic
     val thickPillarBottom = block("thick_pillar_base", "_base", TextureKey.ALL)
-    @JvmStatic
     val thickPillarCenter = block("thick_pillar_center", "_center", TextureKey.ALL)
 
+    //Plank Ladder
+    val plankLadder = block("plank_ladder", TextureKey.ALL)
     /*---------------Living Room Stuff----------------*/
     //Table Models
-    @JvmStatic
     val TABLE_INVENTORY = block("table", "_inventory", TextureKey.TOP, legs)
-    @JvmStatic
     val TABLE_TOP = block("table_top", "_top", TextureKey.TOP)
-    @JvmStatic
     val TABLE_SINGLE_LEG = block("table_single_leg", "_single_leg", legs)
-    @JvmStatic
     val TABLE_END_LEG = block("table_end_leg", "_end_leg", legs)
-    @JvmStatic
     val TABLE_CORNER_LEG = block("table_corner_leg", "_corner_leg", legs)
 
     //Coffee Table Models
-    @JvmStatic
     val coffeeTableInventory: Model = block("coffee_table", "_inventory", TextureKey.TOP, legs)
-    @JvmStatic
     val coffeeTableTopTall: Model = block("coffee_table_top_tall",
         "_top_tall", TextureKey.TOP)
-    @JvmStatic
     val coffeeTableLegTall: Model = block(
         "coffee_table_leg_tall",
         "_leg_tall",
         legs
     )
-    @JvmStatic
     val coffeeTableTopShort: Model =
         block("coffee_table_top_short", "_top_short", TextureKey.TOP)
-    @JvmStatic
     val coffeeTableLegShort: Model = block(
         "coffee_table_leg_short",
         "_leg_short",
@@ -71,16 +54,13 @@ object ModModels {
     /*---------------Kitchen Stuff----------------*/
 
     //Kitchen Counters
-    @JvmStatic
     val kitchenCounter: Model = block("kitchen_counter", TextureKey.SIDE, TextureKey.TOP)
-    @JvmStatic
     val kitchenCounterInnerLeftCorner: Model = block(
         "kitchen_counter_inner_left_corner",
         "_inner_left_corner",
         TextureKey.SIDE,
         TextureKey.TOP
     )
-    @JvmStatic
     val kitchenCounterOuterLeftCorner: Model = block(
         "kitchen_counter_outer_left_corner",
         "_outer_left_corner",
