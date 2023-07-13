546ce27628ba4c9d3d72b842aca957482cf880d7
Mystery2099 <xxmysteryxxftb@gmail.com>
2023-07-13 16:07:47 -0400

    created kitchen cabinet block entity type

--- src/main/kotlin/com/mystery2099/block_entity/ModBlockEntities.kt
+++ src/main/kotlin/com/mystery2099/block_entity/ModBlockEntities.kt
@@ -1,10 +1,24 @@
 package com.mystery2099.block_entity
 
 import com.mystery2099.WoodenAccentsMod
+import com.mystery2099.WoodenAccentsMod.toId
+import com.mystery2099.block.custom.KitchenCabinetBlock
+import com.mystery2099.block_entity.custom.KitchenCabinetBlockEntity
+import net.minecraft.block.entity.BlockEntityType
+import net.minecraft.registry.Registries
+import net.minecraft.registry.Registry
 
 object ModBlockEntities {
 
+    lateinit var kitchenCabinet: BlockEntityType<KitchenCabinetBlockEntity>
+
     fun register() {
+        kitchenCabinet = Registry.register(
+            Registries.BLOCK_ENTITY_TYPE,
+            "kitchen_cabinet".toId(),
+            KitchenCabinetBlock.kitchenCabinetBlockEntityTypeBuilder.build(null)
+        )
+
         WoodenAccentsMod.logger.info("Registering ModBlockEntities for mod: ${WoodenAccentsMod.modid}")
     }
 }
