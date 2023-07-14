b64a8abbba686f2fa208876c3c20b3e939e5399c
Mystery2099 <xxmysteryxxftb@gmail.com>
2023-07-13 21:30:18 -0400

    inverted the direction the kitchen counter faces and changed outline shape stuff

--- src/main/java/com/mystery2099/block/custom/KitchenCounterBlock.java
+++ src/main/java/com/mystery2099/block/custom/KitchenCounterBlock.java
@@ -1,13 +1,9 @@
 package com.mystery2099.block.custom;
 
 import com.mystery2099.WoodenAccentsModItemGroups;
-import com.mystery2099.block.ModBlocks;
 import com.mystery2099.data.ModBlockTags;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.HorizontalFacingBlock;
-import net.minecraft.block.ShapeContext;
+import net.minecraft.block.*;
 import net.minecraft.block.enums.StairShape;
 import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.state.StateManager;
@@ -21,6 +17,7 @@ import net.minecraft.util.math.Direction;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
+import net.minecraft.world.World;
 import net.minecraft.world.WorldAccess;
 import org.jetbrains.annotations.NotNull;
 
@@ -29,12 +26,12 @@ import java.util.Objects;
 public class KitchenCounterBlock extends AbstractWaterloggableBlock{
     public static final EnumProperty<StairShape> SHAPE = Properties.STAIR_SHAPE;
     public static final DirectionProperty FACING = HorizontalFacingBlock.FACING;
-    protected static final VoxelShape TOP_SHAPE = Block.createCuboidShape(0, 14, 0, 16, 16, 16);
-    protected static final VoxelShape SOUTH_SHAPE = Block.createCuboidShape(0, 0, 2, 16, 14, 16);
-    protected static final VoxelShape EAST_SHAPE = Block.createCuboidShape(2, 0, 0, 16, 14, 16);
-    private static final double SHAPE_OFFSET = -((double) 2 / 16);
-    protected static final VoxelShape NORTH_SHAPE = SOUTH_SHAPE.offset(0, 0, SHAPE_OFFSET);
-    protected static final VoxelShape WEST_SHAPE = EAST_SHAPE.offset(SHAPE_OFFSET, 0, 0);
+    public static final VoxelShape TOP_SHAPE = Block.createCuboidShape(0, 14, 0, 16, 16, 16);
+    public static final VoxelShape NORTH_SHAPE = Block.createCuboidShape(0, 0, 2, 16, 14, 16);
+    public static final VoxelShape EAST_SHAPE = Block.createCuboidShape(0, 0, 0, 14, 14, 16);
+    public static final VoxelShape SOUTH_SHAPE = NORTH_SHAPE.offset(0, 0, -((double) 2 / 16));
+    public static final VoxelShape WEST_SHAPE = EAST_SHAPE.offset((double) 2 / 16, 0, 0);
+
     private final Block topBlock, baseBlock;
 
     public KitchenCounterBlock(Block baseBlock, Block topBlock) {
@@ -48,22 +45,25 @@ public class KitchenCounterBlock extends AbstractWaterloggableBlock{
     private static StairShape getCounterShape(BlockState state, BlockView world, BlockPos pos) {
         Direction direction3;
         Direction direction2;
-        var direction = state.get(FACING);
-        var blockState = world.getBlockState(pos.offset(direction));
-        if (canConnectTo(blockState) &&
-                (direction2 = blockState.get(FACING)).getAxis() != state.get(FACING).getAxis() &&
-                isDifferentOrientation(state, world, pos, direction2.getOpposite())) {
-            if (direction2 == direction.rotateYCounterclockwise()) return StairShape.OUTER_LEFT;
+        Direction direction = state.get(FACING);
+        BlockState blockState = world.getBlockState(pos.offset(direction.getOpposite()));
+        if (KitchenCounterBlock.isCounter(blockState) && (direction2 = blockState.get(FACING)).getAxis() != state.get(FACING).getAxis() && KitchenCounterBlock.isDifferentOrientation(state, world, pos, direction2)) {
+            if (direction2 == direction.rotateYCounterclockwise()) {
+                return StairShape.OUTER_LEFT;
+            }
             return StairShape.OUTER_RIGHT;
         }
-        var blockState2 = world.getBlockState(pos.offset(direction.getOpposite()));
-        if (canConnectTo(blockState2) &&
-                (direction3 = blockState2.get(FACING)).getAxis() != state.get(FACING).getAxis() &&
-                isDifferentOrientation(state, world, pos, direction3)) {
-            if (direction3 == direction.rotateYCounterclockwise()) return StairShape.INNER_LEFT;
+        BlockState blockState2 = world.getBlockState(pos.offset(direction));
+        if (KitchenCounterBlock.isCounter(blockState2) && (direction3 = blockState2.get(FACING)).getAxis() != state.get(FACING).getAxis() && KitchenCounterBlock.isDifferentOrientation(state, world, pos, direction3.getOpposite())) {
+            if (direction3 == direction.rotateYCounterclockwise()) {
+                return StairShape.INNER_LEFT;
+            }
             return StairShape.INNER_RIGHT;
         }
         return StairShape.STRAIGHT;
+    }
+    public static boolean isCounter(BlockState state) {
+        return canConnectTo(state);
     }
 
     private static boolean canConnectTo(BlockState blockState) {
@@ -78,47 +78,53 @@ public class KitchenCounterBlock extends AbstractWaterloggableBlock{
     @Override
     @SuppressWarnings("deprecation")
     public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
-        //southShapes
-        var southInnerLeftShape = VoxelShapes.union(SOUTH_SHAPE, Block.createCuboidShape(2, 0, 0, 16, 14, 2));
-        var southInnerRightShape = VoxelShapes.union(SOUTH_SHAPE, Block.createCuboidShape(0, 0, 0, 14, 14, 2));
-        var southOuterLeftShape = Block.createCuboidShape(2, 0, 2, 16, 14, 16);
-        var southOuterRightShape = Block.createCuboidShape(0, 0, 2, 14, 14, 16);
+        var northWest = VoxelShapes.union(NORTH_SHAPE, WEST_SHAPE);
+        var southWest = VoxelShapes.union(SOUTH_SHAPE, WEST_SHAPE);
+        var southEast = VoxelShapes.union(SOUTH_SHAPE, EAST_SHAPE);
+        var northEast = VoxelShapes.union(NORTH_SHAPE, EAST_SHAPE);
 
-        //northShapes
-        var northInnerLeftShape = VoxelShapes.union(NORTH_SHAPE, Block.createCuboidShape(0, 0, 14, 14, 14, 16));
-        var northInnerRightShape = VoxelShapes.union(NORTH_SHAPE, Block.createCuboidShape(2, 0, 14, 16, 14, 16));
-        var northOuterLeftShape = Block.createCuboidShape(0, 0, 0, 14, 14, 14);
-        var northOuterRightShape = Block.createCuboidShape(2, 0, 0, 16, 14, 14);
+        var northOuterLeft = Block.createCuboidShape(0, 0, 2, 14, 14, 16);
+        var northOuterRight = northOuterLeft.offset((double) 2/16, 0, 0);
+
+        var eastOuterLeft = Block.createCuboidShape(0, 0, 0, 14, 14, 14);
+        var eastOuterRight = eastOuterLeft.offset(0, 0, (double) 2/16);
+
+        var southOuterLeft = northOuterRight.offset(0, 0, -((double) 2 / 16));
+        var southOuterRight = northOuterLeft.offset(0, 0, -((double) 2 / 16));
+
+        var westOuterLeft = eastOuterRight.offset((double) 2 / 16, 0, 0);
+        var westOuterRight = eastOuterLeft.offset((double) 2 / 16, 0, 0);
+
 
         var stairShape = state.get(SHAPE);
         return VoxelShapes.union(TOP_SHAPE, switch (state.get(FACING)) {
             case NORTH -> switch (stairShape) {
                 case STRAIGHT -> NORTH_SHAPE;
-                case INNER_LEFT -> northInnerLeftShape;
-                case INNER_RIGHT -> northInnerRightShape;
-                case OUTER_LEFT -> northOuterLeftShape;
-                case OUTER_RIGHT -> northOuterRightShape;
+                case INNER_LEFT -> northWest;
+                case INNER_RIGHT -> northEast;
+                case OUTER_LEFT -> northOuterRight;
+                case OUTER_RIGHT -> northOuterLeft;
             };
             case SOUTH -> switch (stairShape) {
                 case STRAIGHT -> SOUTH_SHAPE;
-                case INNER_LEFT -> southInnerLeftShape;
-                case INNER_RIGHT -> southInnerRightShape;
-                case OUTER_LEFT -> southOuterLeftShape;
-                case OUTER_RIGHT -> southOuterRightShape;
+                case INNER_LEFT -> southEast;
+                case INNER_RIGHT -> southWest;
+                case OUTER_LEFT -> eastOuterLeft;
+                case OUTER_RIGHT -> southOuterLeft;
             };
             case EAST -> switch (stairShape) {
                 case STRAIGHT -> EAST_SHAPE;
-                case INNER_LEFT -> northInnerRightShape;
-                case INNER_RIGHT -> southInnerLeftShape;
-                case OUTER_LEFT -> northOuterRightShape;
-                case OUTER_RIGHT -> southOuterLeftShape;
+                case INNER_LEFT -> northEast;
+                case INNER_RIGHT -> southEast;
+                case OUTER_LEFT -> northOuterLeft;
+                case OUTER_RIGHT -> eastOuterLeft;
             };
             case WEST -> switch (stairShape) {
                 case STRAIGHT -> WEST_SHAPE;
-                case INNER_LEFT -> southInnerRightShape;
-                case INNER_RIGHT -> northInnerLeftShape;
-                case OUTER_LEFT -> southOuterRightShape;
-                case OUTER_RIGHT -> northOuterLeftShape;
+                case INNER_LEFT -> southWest;
+                case INNER_RIGHT -> northWest;
+                case OUTER_LEFT -> southOuterLeft;
+                case OUTER_RIGHT -> northOuterRight;
             };
             default -> VoxelShapes.fullCube();
         });
@@ -133,7 +139,7 @@ public class KitchenCounterBlock extends AbstractWaterloggableBlock{
     @Override
     public BlockState getPlacementState(ItemPlacementContext ctx) {
         var blockPos = ctx.getBlockPos();
-        var blockState = Objects.requireNonNull(super.getPlacementState(ctx)).with(FACING, ctx.getHorizontalPlayerFacing());
+        var blockState = Objects.requireNonNull(super.getPlacementState(ctx)).with(FACING, ctx.getHorizontalPlayerFacing().getOpposite());
         return blockState.with(SHAPE, getCounterShape(blockState, ctx.getWorld(), blockPos));
     }
 
@@ -199,6 +205,19 @@ public class KitchenCounterBlock extends AbstractWaterloggableBlock{
             }
         }
         return super.mirror(state, mirror);
+    }
+
+    @Override
+    public void onBlockAdded(BlockState state, World world, BlockPos pos, BlockState oldState, boolean notify) {
+        if (state.isOf(state.getBlock())) return;
+        world.updateNeighbor(this.baseBlock.getDefaultState(), pos, Blocks.AIR, pos, false);
+        this.baseBlock.onBlockAdded(this.baseBlock.getDefaultState(), world, pos, oldState, false);
+    }
+
+    @Override
+    public void onStateReplaced(BlockState state, World world, BlockPos pos, BlockState newState, boolean moved) {
+        if (state.isOf(newState.getBlock())) return;
+        this.baseBlock.getDefaultState().onStateReplaced(world, pos, newState, moved);
     }
 
     public Block getTopBlock() {
