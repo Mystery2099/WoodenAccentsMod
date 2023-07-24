7a816922f476fd8d62bcab06ec21e2e22321b7cf
Mystery2099 <xxmysteryxxftb@gmail.com>
2023-07-24 15:15:27 -0400

    Made FloorCoveringBlock require the update 1.20 feature flag if the baseBlock requires it.

--- src/main/kotlin/com/mystery2099/block/custom/FloorCoveringBlock.kt
+++ src/main/kotlin/com/mystery2099/block/custom/FloorCoveringBlock.kt
@@ -5,11 +5,15 @@ import net.fabricmc.fabric.api.`object`.builder.v1.block.FabricBlockSettings
 import net.minecraft.block.Block
 import net.minecraft.block.CarpetBlock
 import net.minecraft.block.Material
+import net.minecraft.resource.featuretoggle.FeatureFlags
 
 class FloorCoveringBlock(val baseBlock: Block) : CarpetBlock(
     FabricBlockSettings.of(Material.CARPET).strength(0.1f).apply {
         mapColor(baseBlock.defaultMapColor)
         sounds(baseBlock.getSoundGroup(baseBlock.defaultState))
+        if (baseBlock.requiredFeatures.contains(FeatureFlags.UPDATE_1_20)) {
+            requires(FeatureFlags.UPDATE_1_20)
+        }
     }
 ) {
     init {
