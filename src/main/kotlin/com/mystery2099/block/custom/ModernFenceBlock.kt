13ca47d62d564280af9967a4a12def4fe426ea73
Mystery2099 <xxmysteryxxftb@gmail.com>
2023-07-29 18:26:47 -0400

    started adding outline shapes

--- src/main/kotlin/com/mystery2099/block/custom/ModernFenceBlock.kt
+++ src/main/kotlin/com/mystery2099/block/custom/ModernFenceBlock.kt
@@ -2,12 +2,12 @@ package com.mystery2099.block.custom
 
 import com.mystery2099.data.ModBlockTags
 import net.fabricmc.fabric.api.`object`.builder.v1.block.FabricBlockSettings
-import net.minecraft.block.Block
-import net.minecraft.block.BlockState
-import net.minecraft.block.FenceBlock
-import net.minecraft.block.FenceGateBlock
+import net.minecraft.block.*
 import net.minecraft.registry.tag.BlockTags
+import net.minecraft.util.math.BlockPos
 import net.minecraft.util.math.Direction
+import net.minecraft.util.shape.VoxelShape
+import net.minecraft.world.BlockView
 
 class ModernFenceBlock(settings: Block, val sideBlock: Block, val postBlock: Block) : FenceBlock(FabricBlockSettings.copyOf(settings)) {
 
@@ -16,5 +16,16 @@ class ModernFenceBlock(settings: Block, val sideBlock: Block, val postBlock: Blo
         val bl = state.isIn(BlockTags.FENCES) && state.isIn(ModBlockTags.modernFences) == this.defaultState.isIn(ModBlockTags.modernFences)
         val bl2 = block is FenceGateBlock && FenceGateBlock.canWallConnect(state, dir)
         return !cannotConnect(state) && neighborIsFullSquare || bl || bl2
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun getOutlineShape(
+        state: BlockState?,
+        world: BlockView?,
+        pos: BlockPos?,
+        context: ShapeContext?
+    ): VoxelShape {
+
+        return super.getOutlineShape(state, world, pos, context)
     }
 }
--- src/main/kotlin/com/mystery2099/block/custom/ModernFenceGateBlock.kt
+++ src/main/kotlin/com/mystery2099/block/custom/ModernFenceGateBlock.kt
@@ -3,7 +3,36 @@ package com.mystery2099.block.custom
 import com.mystery2099.WoodenAccentsMod.woodType
 import net.fabricmc.fabric.api.`object`.builder.v1.block.FabricBlockSettings
 import net.minecraft.block.Block
+import net.minecraft.block.BlockState
 import net.minecraft.block.FenceGateBlock
+import net.minecraft.block.ShapeContext
+import net.minecraft.util.math.BlockPos
+import net.minecraft.util.math.Direction
+import net.minecraft.util.shape.VoxelShape
+import net.minecraft.util.shape.VoxelShapes
+import net.minecraft.world.BlockView
 
 class ModernFenceGateBlock(baseGate: FenceGateBlock, val baseBlock: Block) : FenceGateBlock(FabricBlockSettings.copyOf(baseGate), baseGate.woodType()) {
+    @Deprecated("Deprecated in Java")
+    override fun getOutlineShape(
+        state: BlockState,
+        world: BlockView?,
+        pos: BlockPos?,
+        context: ShapeContext?
+    ): VoxelShape {
+        val northSouthShape = VoxelShapes.union(
+            Block.createCuboidShape(0.0, 0.0, 7.5, 1.0, 15.0, 8.5),
+            Block.createCuboidShape(15.0, 0.0, 7.5, 16.0, 15.0, 8.5),
+            Block.createCuboidShape(11.0, 1.0, 7.5, 13.0, 16.0, 8.5),
+            Block.createCuboidShape(3.0, 1.0, 7.5, 5.0, 16.0, 8.5),
+            Block.createCuboidShape(7.0, 1.0, 7.5, 9.0, 15.0, 8.5),
+            Block.createCuboidShape(0.0, 11.0, 7.0, 16.0, 14.0, 9.0),
+            Block.createCuboidShape(0.0, 2.0, 7.0, 16.0, 5.0, 9.0),
+            Block.createCuboidShape(0.0, 2.0, 7.5, 16.0, 14.0, 8.5)
+        )
+        return when (state.get(FACING)) {
+            Direction.NORTH, Direction.SOUTH -> northSouthShape
+            else -> super.getOutlineShape(state, world, pos, context)
+        }
+    }
 }
