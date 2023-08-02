a32a4d2a581af32871c2313f4d37b028b31e285a
Mystery2099 <xxmysteryxxftb@gmail.com>
2023-08-02 16:12:59 -0400

    Added methods for working with arrays. replaced the use of the Direction enum with a new enum. Made a few other changed

--- src/main/kotlin/com/mystery2099/util/VoxelShapeHelper.kt
+++ src/main/kotlin/com/mystery2099/util/VoxelShapeHelper.kt
@@ -1,6 +1,5 @@
 package com.mystery2099.util
 
-import net.minecraft.util.function.BooleanBiFunction
 import net.minecraft.util.math.Direction
 import net.minecraft.util.shape.VoxelShape
 import net.minecraft.util.shape.VoxelShapes
@@ -9,31 +8,40 @@ import kotlin.math.max
 import kotlin.math.min
 
 object VoxelShapeHelper {
+    private val Double.limited: Double
+        get() = this.limit()
     val Collection<VoxelShape>.combined: VoxelShape
         get() = this.union()
-    fun Collection<VoxelShape>.union(): VoxelShape = this.fold(VoxelShapes.empty(), VoxelShapes::union)
+    val Array<VoxelShape>.combined: VoxelShape
+        get() = this.union()
 
-    fun VoxelShape.union(vararg otherShapes: VoxelShape): VoxelShape = otherShapes.fold(this, VoxelShapes::union)
-    fun VoxelShape.union(otherShapes: Collection<VoxelShape>): VoxelShape = otherShapes.fold(this, VoxelShapes::union);
+    fun Collection<VoxelShape>.union(): VoxelShape = this.fold(VoxelShapes.empty(), VoxelShapes::union)
+    fun Array<VoxelShape>.union(): VoxelShape = this.fold(VoxelShapes.empty(), VoxelShapes::union)
+
+    fun VoxelShape.unionWith(otherShape: VoxelShape): VoxelShape = VoxelShapes.union(this, otherShape)
+    fun VoxelShape.unionWith(vararg otherShapes: VoxelShape): VoxelShape = otherShapes.fold(this, VoxelShapes::union)
+    fun VoxelShape.unionWith(otherShapes: Collection<VoxelShape>): VoxelShape = this.unionWith(otherShapes.combined)
+
     fun setMaxHeight(source: VoxelShape, height: Double): VoxelShape {
         val result = AtomicReference(VoxelShapes.empty())
         source.forEachBox { minX: Double, minY: Double, minZ: Double, maxX: Double, maxY: Double, maxZ: Double ->
             val shape = VoxelShapes.cuboid(minX, minY, minZ, maxX, height, maxZ)
-            result.set(VoxelShapes.combine(result.get(), shape, BooleanBiFunction.OR))
+            result.set(result.get().unionWith(shape))
         }
-        return result.get().simplify()
+        return result.get()
     }
 
     fun limitHorizontal(source: VoxelShape): VoxelShape {
         val result = AtomicReference(VoxelShapes.empty())
         source.forEachBox { minX: Double, minY: Double, minZ: Double, maxX: Double, maxY: Double, maxZ: Double ->
             val shape = VoxelShapes.cuboid(minX.limited, minY, minZ.limited, maxX.limited, maxY, maxZ.limited)
-            result.set(VoxelShapes.combine(result.get(), shape, BooleanBiFunction.OR))
+            result.set(result.get().unionWith(shape))
         }
-        return result.get().simplify()
+        return result.get()
     }
 
-    fun VoxelShape.rotate(direction: Direction): VoxelShape {
+    //Do not use on complex VoxelShapes
+    fun VoxelShape.rotate(direction: VoxelShapeRotations): VoxelShape {
         val adjustedValues = adjustValues(
             direction, this.getMin(Direction.Axis.X), this.getMin(Direction.Axis.Z), this.getMax(
                 Direction.Axis.X
@@ -45,26 +53,59 @@ object VoxelShapeHelper {
         )
     }
 
-    fun Collection<VoxelShape>.rotate(direction: Direction): VoxelShape {
-        return this.fold(VoxelShapes.empty()) {  acc, shape -> VoxelShapes.union(acc, shape.rotate(direction)) }
+    //Do not use on complex VoxelShapes
+    fun VoxelShape.rotateLeft(): VoxelShape = rotate(VoxelShapeRotations.LEFT)
+
+    //Do not use on complex VoxelShapes
+    fun VoxelShape.rotateRight(): VoxelShape = rotate(VoxelShapeRotations.RIGHT)
+
+    //Do not use on complex VoxelShapes
+    fun VoxelShape.flip(): VoxelShape = rotate(VoxelShapeRotations.FLIP)
+
+    fun Collection<VoxelShape>.rotateElements(direction: VoxelShapeRotations): Collection<VoxelShape> {
+        if (isEmpty()) {
+            val callerStackTrace = Thread.currentThread().stackTrace[2]
+            val callerClassName = callerStackTrace.className
+            val callerLineNumber = callerStackTrace.lineNumber
+            println("Warning: Collection of VoxelShapes is empty in class: $callerClassName, line: $callerLineNumber. Returning a list containing an empty VoxelShape.")
+            return listOf(VoxelShapes.empty())
+        }
+        return map { it.rotate(direction) }
+    }
+    fun Array<VoxelShape>.rotateElements(direction: VoxelShapeRotations): Array<VoxelShape> {
+        if (isEmpty()) {
+            val callerStackTrace = Thread.currentThread().stackTrace[2]
+            val callerClassName = callerStackTrace.className
+            val callerLineNumber = callerStackTrace.lineNumber
+            println("Warning: Array of VoxelShapes is empty in class: $callerClassName, line: $callerLineNumber. Returning an Array containing an empty VoxelShape.")
+            return arrayOf(VoxelShapes.empty())
+        }
+        return map { it.rotate(direction) }.toTypedArray()
+    }
+
+    fun Collection<VoxelShape>.rotateAndCombine(direction: VoxelShapeRotations): VoxelShape {
+        return rotateElements(direction).combined
+    }
+    fun Array<VoxelShape>.rotateAndCombine(direction: VoxelShapeRotations): VoxelShape {
+        return rotateElements(direction).combined
     }
 
     private fun adjustValues(
-        direction: Direction,
+        direction: VoxelShapeRotations,
         var1: Double,
         var2: Double,
         var3: Double,
         var4: Double
-    ): DoubleArray {
-        return when (direction) {
-            Direction.WEST -> doubleArrayOf(1.0f - var3, 1.0f - var4, 1.0f - var1, 1.0f - var2)
-            Direction.NORTH -> doubleArrayOf(var2, 1.0f - var3, var4, 1.0f - var1)
-            Direction.SOUTH -> doubleArrayOf(1.0f - var4, var1, 1.0f - var2, var3)
-            else -> doubleArrayOf(var1, var2, var3, var4)
-        }
+    ): DoubleArray = when (direction) {
+        VoxelShapeRotations.FLIP -> doubleArrayOf(1.0f - var3, 1.0f - var4, 1.0f - var1, 1.0f - var2)
+        VoxelShapeRotations.RIGHT -> doubleArrayOf(var2, 1.0f - var3, var4, 1.0f - var1)
+        VoxelShapeRotations.LEFT -> doubleArrayOf(1.0f - var4, var1, 1.0f - var2, var3)
+        else -> doubleArrayOf(var1, var2, var3, var4)
     }
 
-    private val Double.limited: Double
-        get() = this.limit()
     private fun Double.limit(): Double = max(0.0, min(1.0, this))
+
+}
+enum class VoxelShapeRotations {
+    LEFT, RIGHT, FLIP, UP, DOWN
 }
