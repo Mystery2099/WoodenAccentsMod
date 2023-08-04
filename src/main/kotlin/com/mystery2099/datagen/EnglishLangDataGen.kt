f65026e52d4d7610abbe3d0bf12421cddf762f08
Mystery2099 <xxmysteryxxftb@gmail.com>
2023-08-04 18:14:08 -0400

    Overhalled String.toName() and made some adjustments to other methods

--- src/main/kotlin/com/mystery2099/datagen/EnglishLangDataGen.kt
+++ src/main/kotlin/com/mystery2099/datagen/EnglishLangDataGen.kt
@@ -2,38 +2,28 @@ package com.mystery2099.datagen
 
 import com.mystery2099.WoodenAccentsModItemGroups
 import com.mystery2099.block.ModBlocks
-import com.mystery2099.block.custom.CoffeeTableBlock
-import com.mystery2099.block.custom.enums.CoffeeTableType
-import com.mystery2099.state.property.ModProperties
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricLanguageProvider
 import net.minecraft.block.Block
 import net.minecraft.item.ItemGroup
 
 class EnglishLangDataGen(dataOutput: FabricDataOutput) : FabricLanguageProvider(dataOutput) {
-    private var builder: TranslationBuilder? = null
     override fun generateTranslations(translationBuilder: TranslationBuilder) {
-        builder = translationBuilder
-
-        ModBlocks.blocks.forEach(::genLangName)
-
-        WoodenAccentsModItemGroups.itemGroups.forEach(::genLangName)
-
-    }
-
-    private fun genLangName(itemGroup: ItemGroup) {
-        builder!!.add(itemGroup, "Wooden Accents: ${itemGroup.id.path.toName()}")
-    }
-    private fun genLangName(block: Block) {
-        builder!!.add(block, block.lootTableId.path.replace("blocks/", "").toName())
-    }
-
-    private fun String.toName(): String {
-        val parts = this.split("_")
-        var output = ""
-        parts.forEach { str ->
-            output += str.replaceFirstChar { it.uppercase() }.replace("_", " ") + " "
+        with(translationBuilder) {
+            ModBlocks.blocks.forEach{it.genLangName(this)}
+            WoodenAccentsModItemGroups.itemGroups.forEach{it.genLangName(this)}
         }
-        return output.removeSuffix(" ")
+
+    }
+
+    private fun ItemGroup.genLangName(translationBuilder: TranslationBuilder) {
+        translationBuilder.add(this, "Wooden Accents: ${this.id.path.toName()}")
+    }
+    private fun Block.genLangName(translationBuilder: TranslationBuilder) {
+        translationBuilder.add(this, this.lootTableId.path.replace("blocks/", "").toName())
+    }
+    private fun String?.toName(): String {
+        return if (isNullOrEmpty()) ""
+        else split("_").joinToString(" ") { it.replaceFirstChar(Char::uppercase) }
     }
 }
