ad4e1740cd4b504d4fdfb6f20e6430d6a9e799ab
Mystery2099 <xxmysteryxxftb@gmail.com>
2023-08-05 16:27:51 -0400

    Renamed unionWith functions to uvifiedWith() and added uvLock() function

--- src/main/kotlin/com/mystery2099/wooden_accents_mod/util/BlockStateVariantUtil.kt
+++ src/main/kotlin/com/mystery2099/wooden_accents_mod/util/BlockStateVariantUtil.kt
@@ -1,22 +1,24 @@
 package com.mystery2099.wooden_accents_mod.util
 
 import net.minecraft.data.client.BlockStateVariant
+import net.minecraft.data.client.VariantSetting
 import net.minecraft.data.client.VariantSettings
 import net.minecraft.util.Identifier
 
 object BlockStateVariantUtil {
-    infix fun BlockStateVariant.unionWith(other: BlockStateVariant): BlockStateVariant = BlockStateVariant.union(this, other)
+    infix fun BlockStateVariant.unifiedWith(other: BlockStateVariant): BlockStateVariant = BlockStateVariant.union(this, other)
+    fun BlockStateVariant.unifiedWith(vararg others: BlockStateVariant) = others.fold(this, BlockStateVariant::union)
+    infix fun BlockStateVariant.plus(other: BlockStateVariant) = unifiedWith(other)
 
-    fun BlockStateVariant.unionWith(vararg others: BlockStateVariant) = others.fold(this, BlockStateVariant::union)
-    infix fun BlockStateVariant.plus(other: BlockStateVariant) = unionWith(other)
+    infix fun BlockStateVariant.putModel(model: Identifier): BlockStateVariant = this.put(VariantSettings.MODEL, model)
+    fun Identifier.asBlockStateVariant() = BlockStateVariant().putModel(this)
 
-    infix fun BlockStateVariant.putModel(model: Identifier): BlockStateVariant = put(VariantSettings.MODEL, model)
-    infix fun Identifier.asBlockStateVariant() = BlockStateVariant().putModel(this)
-
-    infix fun BlockStateVariant.withYRotationOf(rotation: VariantSettings.Rotation) = unionWith(
+    infix fun BlockStateVariant.withYRotationOf(rotation: VariantSettings.Rotation) = unifiedWith(
         BlockStateVariant().put(VariantSettings.Y, rotation)
     )
-    infix fun BlockStateVariant.withXRotationOf(rotation: VariantSettings.Rotation) = unionWith(
+    infix fun BlockStateVariant.withXRotationOf(rotation: VariantSettings.Rotation) = unifiedWith(
         BlockStateVariant().put(VariantSettings.X, rotation)
     )
+    fun BlockStateVariant.uvLock(): BlockStateVariant = put(VariantSettings.UVLOCK, true)
+    operator fun <T> BlockStateVariant.set(key: VariantSetting<T>, value: T): BlockStateVariant = put(key, value)
 }
