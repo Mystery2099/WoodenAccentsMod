053cba53c5dd9e7509a97247c872e22a945c493a
Mystery2099 <54286204+Mystery2099@users.noreply.github.com>
2023-08-05 12:23:55 -0400

    Update BlockStateVariantUtil.kt
    Added infix to some functions

--- src/main/kotlin/com/mystery2099/wooden_accents_mod/util/BlockStateVariantUtil.kt
+++ src/main/kotlin/com/mystery2099/wooden_accents_mod/util/BlockStateVariantUtil.kt
@@ -5,18 +5,18 @@ import net.minecraft.data.client.VariantSettings
 import net.minecraft.util.Identifier
 
 object BlockStateVariantUtil {
-    fun BlockStateVariant.unionWith(other: BlockStateVariant): BlockStateVariant = BlockStateVariant.union(this, other)
+    infix fun BlockStateVariant.unionWith(other: BlockStateVariant): BlockStateVariant = BlockStateVariant.union(this, other)
 
     fun BlockStateVariant.unionWith(vararg others: BlockStateVariant) = others.fold(this, BlockStateVariant::union)
-    fun BlockStateVariant.plus(other: BlockStateVariant) = unionWith(other)
+    infix fun BlockStateVariant.plus(other: BlockStateVariant) = unionWith(other)
 
-    fun BlockStateVariant.putModel(model: Identifier): BlockStateVariant = put(VariantSettings.MODEL, model)
-    fun Identifier.asBlockStateVariant() = BlockStateVariant().putModel(this)
+    infix fun BlockStateVariant.putModel(model: Identifier): BlockStateVariant = put(VariantSettings.MODEL, model)
+    infix fun Identifier.asBlockStateVariant() = BlockStateVariant().putModel(this)
 
-    fun BlockStateVariant.withYRotationOf(rotation: VariantSettings.Rotation) = unionWith(
+    infix fun BlockStateVariant.withYRotationOf(rotation: VariantSettings.Rotation) = unionWith(
         BlockStateVariant().put(VariantSettings.Y, rotation)
     )
-    fun BlockStateVariant.withXRotationOf(rotation: VariantSettings.Rotation) = unionWith(
+    infix fun BlockStateVariant.withXRotationOf(rotation: VariantSettings.Rotation) = unionWith(
         BlockStateVariant().put(VariantSettings.X, rotation)
     )
 }
