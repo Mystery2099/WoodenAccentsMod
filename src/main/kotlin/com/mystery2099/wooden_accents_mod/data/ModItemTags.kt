a370a51fe51ba7046ca5245f7b20e152df9e09bc
Mystery2099 <xxmysteryxxftb@gmail.com>
2023-08-12 19:05:58 -0400

    overrided operator fun contains for being used to check if an ItemStack is in a tag

--- src/main/kotlin/com/mystery2099/wooden_accents_mod/data/ModItemTags.kt
+++ src/main/kotlin/com/mystery2099/wooden_accents_mod/data/ModItemTags.kt
@@ -3,6 +3,7 @@ package com.mystery2099.wooden_accents_mod.data
 import com.mystery2099.wooden_accents_mod.WoodenAccentsMod
 import com.mystery2099.wooden_accents_mod.WoodenAccentsMod.asPathIn
 import net.minecraft.item.Item
+import net.minecraft.item.ItemStack
 import net.minecraft.registry.RegistryKeys
 import net.minecraft.registry.tag.TagKey
 
@@ -12,5 +13,6 @@ object ModItemTags {
     val uncrateable = "uncrateable".toItemTag()
 
     private infix fun String.asItemTagOf(namespace: String): TagKey<Item> = TagKey.of(RegistryKeys.ITEM, asPathIn(namespace))
-    internal fun String.toItemTag(): TagKey<Item> = asItemTagOf(WoodenAccentsMod.MOD_ID)
+    private fun String.toItemTag(): TagKey<Item> = asItemTagOf(WoodenAccentsMod.MOD_ID)
+    operator fun TagKey<Item>.contains(stack: ItemStack): Boolean = stack.isIn(this)
 }
