a98927bf6ac0d82d94b29a08ddf86d8e7dab496f
Mystery2099 <xxmysteryxxftb@gmail.com>
2023-08-12 18:57:56 -0400

    Overrided canInsertIntoSlot and rewrote quickMove

--- src/main/kotlin/com/mystery2099/wooden_accents_mod/screen/CrateScreenHandler.kt
+++ src/main/kotlin/com/mystery2099/wooden_accents_mod/screen/CrateScreenHandler.kt
@@ -21,38 +21,32 @@ class CrateScreenHandler @JvmOverloads constructor(
         checkSize(inventory, 9)
         inventory.onOpen(playerInventory.player)
         for (i in 0 until 3) for (j in 0 until 3) {
-            this.addSlot(ShulkerBoxSlot(inventory, j + i * 3, 62 + j * 18, 17 + i * 18))
+            addSlot(ShulkerBoxSlot(inventory, j + i * 3, 62 + j * 18, 17 + i * 18))
         }
         for (i in 0 until 3) for (j in 0 until 9) {
-            this.addSlot(Slot(playerInventory, j + i * 9 + 9, 8 + j * 18, 84 + i * 18))
+            addSlot(Slot(playerInventory, j + i * 9 + 9, 8 + j * 18, 84 + i * 18))
         }
         for (i in 0 until 9) {
-            this.addSlot(Slot(playerInventory, i, 8 + i * 18, 142))
+            addSlot(Slot(playerInventory, i, 8 + i * 18, 142))
         }
     }
 
     override fun canUse(player: PlayerEntity): Boolean = inventory.canPlayerUse(player)
-
+    override fun canInsertIntoSlot(stack: ItemStack, slot: Slot): Boolean = slot.canInsert(stack)
     override fun quickMove(player: PlayerEntity, slot: Int): ItemStack {
         var itemStack = ItemStack.EMPTY
-        val slot2 = slots[slot]
+        val slot2 = this.slots[slot]
         if (slot2.hasStack()) {
             val itemStack2 = slot2.stack
             itemStack = itemStack2.copy()
-            if (if (slot < inventory.size()) {
-                    !insertItem(
-                        itemStack2,
-                        inventory.size(),
-                        slots.size,
-                        true
-                    )
-            } else !insertItem(itemStack2, 0, inventory.size(), false)
-            ) {
-                return ItemStack.EMPTY
-            }
-            if (itemStack2.isEmpty()) slot2.stack = ItemStack.EMPTY else slot2.markDirty()
+            if (!when {
+                slot < this.inventory.size() -> insertItem(itemStack2, this.inventory.size(), this.slots.size, true)
+                    else -> insertItem(itemStack2, 0, this.inventory.size(), false)
+            }) return ItemStack.EMPTY
+            if(itemStack2.isEmpty) slot2.stack = ItemStack.EMPTY
+            else slot2.markDirty()
         }
-        return itemStack!!
+        return itemStack
     }
 
     override fun onClosed(player: PlayerEntity) {
