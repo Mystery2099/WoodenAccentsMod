bc8acaad6d6a7155fa4b161aa93765c1d9e1d47e
Mystery2099 <xxmysteryxxftb@gmail.com>
2023-08-15 18:42:40 -0400

    created CratePreviewProvider and created a ShulkerBoxTooltip plugin and register crates and kitchen cabinets to it

---
+++ src/main/kotlin/com/mystery2099/wooden_accents_mod/shulker_box_tooltip/ShulkerBoxTooltipPlugin.kt
@@ -0,0 +1,28 @@
+package com.mystery2099.wooden_accents_mod.shulker_box_tooltip
+
+import com.misterpemodder.shulkerboxtooltip.api.ShulkerBoxTooltipApi
+import com.misterpemodder.shulkerboxtooltip.api.provider.BlockEntityPreviewProvider
+import com.misterpemodder.shulkerboxtooltip.api.provider.PreviewProviderRegistry
+import com.mystery2099.wooden_accents_mod.WoodenAccentsMod.toIdentifier
+import com.mystery2099.wooden_accents_mod.block.ModBlocks
+import com.mystery2099.wooden_accents_mod.block.custom.CrateBlock
+import com.mystery2099.wooden_accents_mod.block.custom.KitchenCabinetBlock
+
+object ShulkerBoxTooltipPlugin : ShulkerBoxTooltipApi {
+    override fun registerProviders(registry: PreviewProviderRegistry) {
+        ModBlocks.blocks.forEach{ block ->
+            when (block) {
+                is CrateBlock -> {
+                    registry.register("crates".toIdentifier(),
+                        CratePreviewProvider(),
+                        block.asItem())
+                }
+                is KitchenCabinetBlock -> {
+                    registry.register("chest_like".toIdentifier(),
+                        BlockEntityPreviewProvider(27, true),
+                        block.asItem())
+                }
+            }
+        }
+    }
+}
