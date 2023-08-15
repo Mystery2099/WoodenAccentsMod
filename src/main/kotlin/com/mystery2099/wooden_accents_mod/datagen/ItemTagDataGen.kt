6d66740198986cdabb3393fd1f3a246c4e820209
Mystery2099 <xxmysteryxxftb@gmail.com>
2023-08-15 14:50:41 -0400

    Added bundle to #myst2099_wooden_accents_mod:uncrateable

--- src/main/kotlin/com/mystery2099/wooden_accents_mod/datagen/ItemTagDataGen.kt
+++ src/main/kotlin/com/mystery2099/wooden_accents_mod/datagen/ItemTagDataGen.kt
@@ -5,6 +5,7 @@ import com.mystery2099.wooden_accents_mod.data.ModItemTags
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider
 import net.fabricmc.fabric.api.tag.convention.v1.ConventionalItemTags
+import net.minecraft.item.Items
 import net.minecraft.registry.RegistryWrapper
 import java.util.concurrent.CompletableFuture
 
@@ -18,6 +19,7 @@ class ItemTagDataGen(
         getOrCreateTagBuilder(ModItemTags.uncrateable).apply {
             addTag(ModBlockTags.blockTagWithMatchingItemTag[ModBlockTags.crates])
             forceAddTag(ConventionalItemTags.SHULKER_BOXES)
+            add(Items.BUNDLE)
         }
     }
 }
