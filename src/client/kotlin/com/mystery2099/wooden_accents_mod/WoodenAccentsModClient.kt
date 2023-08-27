94e3be0d97464bf849354086276c370bd045bbde
Mystery2099 <xxmysteryxxftb@gmail.com>
2023-08-27 17:18:14 -0400

    Aded model predicate providers for all coffee tables

--- src/client/kotlin/com/mystery2099/wooden_accents_mod/WoodenAccentsModClient.kt
+++ src/client/kotlin/com/mystery2099/wooden_accents_mod/WoodenAccentsModClient.kt
@@ -1,20 +1,27 @@
 package com.mystery2099.wooden_accents_mod
 
-import com.mystery2099.wooden_accents_mod.WoodenAccentsMod.toIdentifier
 import com.mystery2099.wooden_accents_mod.block.ModBlocks
+import com.mystery2099.wooden_accents_mod.block.custom.CoffeeTableBlock
+import com.mystery2099.wooden_accents_mod.block.custom.enums.CoffeeTableType
 import net.fabricmc.api.ClientModInitializer
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap
 import net.minecraft.client.item.ModelPredicateProviderRegistry
+import net.minecraft.util.Identifier
 
 object WoodenAccentsModClient : ClientModInitializer {
-	override fun onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-		BlockRenderLayerMap.INSTANCE.apply {
+    override fun onInitializeClient() {
+        // This entrypoint is suitable for setting up client-specific logic, such as rendering.
+        BlockRenderLayerMap.INSTANCE.apply {
 
-		}
-		ModelPredicateProviderRegistry.register(ModBlocks.oakCoffeeTable.asItem(), "height".toIdentifier()
-		) { itemStack, _, _, _ ->
-			if (itemStack.nbt?.getBoolean("tall") == true) 1.0f else 0.0f
-		}
-	}
+        }
+        ModBlocks.blocks.forEach {
+			if (it is CoffeeTableBlock) {
+				ModelPredicateProviderRegistry.register(
+					it.asItem(), Identifier("height")
+				) { itemStack, _, _, _ ->
+					if (itemStack.nbt?.getString("coffee_table_type") != CoffeeTableType.TALL.asString()) 0.5f else 1.0f
+				}
+			}
+        }
+    }
 }
