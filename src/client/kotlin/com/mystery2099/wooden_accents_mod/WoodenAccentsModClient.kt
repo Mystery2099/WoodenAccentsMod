5fc5f09f1b9e675832ec7d7e27de74162c77081b
Mystery2099 <xxmysteryxxftb@gmail.com>
2023-08-26 20:08:47 -0400

    got instance of BlockRenderLayerMap for future use

--- src/client/kotlin/com/mystery2099/wooden_accents_mod/WoodenAccentsModClient.kt
+++ src/client/kotlin/com/mystery2099/wooden_accents_mod/WoodenAccentsModClient.kt
@@ -1,9 +1,13 @@
 package com.mystery2099.wooden_accents_mod
 
 import net.fabricmc.api.ClientModInitializer
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap
 
 object WoodenAccentsModClient : ClientModInitializer {
 	override fun onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+		BlockRenderLayerMap.INSTANCE.apply {
+
+		}
 	}
 }
