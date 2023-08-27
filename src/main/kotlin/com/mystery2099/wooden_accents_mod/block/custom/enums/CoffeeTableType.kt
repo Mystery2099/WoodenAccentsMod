6b49440670289db6d4d33d74e100c2917fc742c9
Mystery2099 <xxmysteryxxftb@gmail.com>
2023-08-26 21:33:28 -0400

    Cleaned up code a little

--- src/main/kotlin/com/mystery2099/wooden_accents_mod/block/custom/enums/CoffeeTableType.kt
+++ src/main/kotlin/com/mystery2099/wooden_accents_mod/block/custom/enums/CoffeeTableType.kt
@@ -3,12 +3,10 @@ package com.mystery2099.wooden_accents_mod.block.custom.enums
 import net.minecraft.util.StringIdentifiable
 
 
-enum class CoffeeTableType(string: String) : StringIdentifiable {
+enum class CoffeeTableType(private val string: String) : StringIdentifiable {
     SHORT("short"),
     TALL("tall");
 
-    private val string: String
-    init { this.string = string }
     override fun asString() = string
 }
 
