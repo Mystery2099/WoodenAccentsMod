a03c1c7a1c55a2d5b2cb4498f57c467dba453120
Mystery2099 <xxmysteryxxftb@gmail.com>
2023-09-01 13:19:50 -0400

    replaced lootTableBuilder variable with function to allow parameters

--- src/main/kotlin/com/mystery2099/wooden_accents_mod/block/custom/interfaces/CustomLootTableProvider.kt
+++ src/main/kotlin/com/mystery2099/wooden_accents_mod/block/custom/interfaces/CustomLootTableProvider.kt
@@ -1,7 +1,8 @@
 package com.mystery2099.wooden_accents_mod.block.custom.interfaces
 
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricBlockLootTableProvider
 import net.minecraft.loot.LootTable
 
 interface CustomLootTableProvider {
-    val lootTableBuilder: LootTable.Builder
+    fun getLootTableBuilder(provider: FabricBlockLootTableProvider): LootTable.Builder
 }
