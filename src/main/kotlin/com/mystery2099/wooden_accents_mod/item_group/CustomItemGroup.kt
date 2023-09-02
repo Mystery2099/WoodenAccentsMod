a409e03855d7e14b1a0ac78a5ad316dcfd517894
Mystery2099 <xxmysteryxxftb@gmail.com>
2023-09-02 19:20:55 -0400

    Rewrote entries getter and added functionality for adding the alternate versions of blocks like the tall coffee table to be after the rest of the normal ones. So all tall coffee tables will be after the normal ones

--- src/main/kotlin/com/mystery2099/wooden_accents_mod/item_group/CustomItemGroup.kt
+++ src/main/kotlin/com/mystery2099/wooden_accents_mod/item_group/CustomItemGroup.kt
@@ -3,7 +3,10 @@ package com.mystery2099.wooden_accents_mod.item_group
 import com.mystery2099.wooden_accents_mod.WoodenAccentsMod.toIdentifier
 import com.mystery2099.wooden_accents_mod.block.ModBlocks
 import com.mystery2099.wooden_accents_mod.block.custom.interfaces.CustomItemGroupProvider
+import com.mystery2099.wooden_accents_mod.block.defaultItemStack
+import com.mystery2099.wooden_accents_mod.item.CustomBlockItem
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup
+import net.minecraft.block.Block
 import net.minecraft.item.ItemGroup
 import net.minecraft.item.ItemStack
 import net.minecraft.item.Items
@@ -11,21 +14,30 @@ import net.minecraft.item.Items
 class CustomItemGroup(name: String) {
     val itemGroup: ItemGroup
     inline val entries: List<ItemStack>
-        get() = run {
+        get() {
             val list = mutableListOf<ItemStack>()
-            ModBlocks.blocks.forEach { block ->
-                if (block is CustomItemGroupProvider) {
-                    if (block.itemGroup == this) {
-                        list += block.asItem().defaultStack
-                        if (block.hasNbtVariant) {
-                            list += block.variantWithNbt
-                        }
+            val altList = mutableListOf<ItemStack>()
+
+            ModBlocks.blocks.filterIsInstance<CustomItemGroupProvider>()
+                .filter { it.itemGroup == this }
+                .forEach { block ->
+                    list += if (block is Block) block.defaultItemStack else ItemStack.EMPTY
+                    if (block.hasVariantItemGroupStack) {
+                        altList += block.variantItemGroupStack
                     }
                 }
 
+            altList.forEach { alt ->
+                val element = list.lastOrNull {
+                    (it.item as CustomBlockItem).tagFromProvider == (alt.item as CustomBlockItem).tagFromProvider
+                }
+                val index = list.indexOf(element)
+                if (index > -1) list.add(index + 1, alt)
             }
+
             return list.ifEmpty { listOf(Items.DIRT.defaultStack) }
         }
+
     init {
         itemGroup = FabricItemGroup.builder(name.toIdentifier()).apply {
             icon { entries[0] }
