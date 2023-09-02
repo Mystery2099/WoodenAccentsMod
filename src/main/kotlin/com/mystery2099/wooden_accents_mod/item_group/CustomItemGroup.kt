1c93c430ae539322d4a047cb6dbe7034d43cb3fb
Mystery2099 <xxmysteryxxftb@gmail.com>
2023-09-02 17:11:31 -0400

    removed unnecessary variables and functions that could be accessed via the itemGroup variable. Added get() function which returns the itemGroup variable, since this is kind of a wrapper for that.

--- src/main/kotlin/com/mystery2099/wooden_accents_mod/item_group/CustomItemGroup.kt
+++ src/main/kotlin/com/mystery2099/wooden_accents_mod/item_group/CustomItemGroup.kt
@@ -8,8 +8,6 @@ import net.minecraft.item.ItemConvertible
 import net.minecraft.item.ItemGroup
 import net.minecraft.item.ItemStack
 import net.minecraft.item.Items
-import net.minecraft.text.Text
-import java.util.function.Consumer
 
 class CustomItemGroup(name: String) {
     val itemGroup: ItemGroup
@@ -21,60 +19,16 @@ class CustomItemGroup(name: String) {
             }
             return list.ifEmpty { listOf(Items.DIRT) }
         }
-    inline val displayName: Text
-        get() = itemGroup.displayName
-
-    inline val icon : ItemStack
-        get() = itemGroup.icon
-
-    inline val texture: String
-        get() = itemGroup.texture
-
-    inline val shouldRenderName: Boolean
-        get() = itemGroup.shouldRenderName()
-
-    inline val hasScrollbar: Boolean
-        get() = itemGroup.hasScrollbar()
-
-    inline val column: Int
-        get() = itemGroup.column
-
-    inline val row: ItemGroup.Row
-        get() = itemGroup.row
-
-    inline val hasStacks: Boolean
-        get() = itemGroup.hasStacks()
-
-    inline val shouldDisplay: Boolean
-        get() = itemGroup.shouldDisplay()
-
-    inline val isSpecial: Boolean
-        get() = itemGroup.isSpecial
-
-    val type: ItemGroup.Type
-        get() = itemGroup.type
-    inline val displayStacks: Collection<ItemStack>
-        get() = itemGroup.displayStacks
-
-    inline val searchTabStacks: Collection<ItemStack>
-        get() = itemGroup.searchTabStacks
 
     init {
         itemGroup = FabricItemGroup.builder(name.toIdentifier()).apply {
-            icon { ItemStack(if (entries.isNotEmpty()) entries[0] else Items.DIRT) }
+            icon { ItemStack(entries[0]) }
         }.build()
         mutableInstances += this
     }
-
-    fun updateEntries(displayContext: ItemGroup.DisplayContext) = itemGroup.updateEntries(displayContext)
+    fun get() = itemGroup
 
     infix operator fun contains(stack: ItemStack) = itemGroup.contains(stack)
-
-    infix fun setSearchProviderReloader(searchProviderReloader: Consumer<List<ItemStack>>) {
-        itemGroup.setSearchProviderReloader(searchProviderReloader)
-    }
-
-    fun reloadSearchProvider() = itemGroup.reloadSearchProvider()
 
     companion object {
         val instances: List<CustomItemGroup>
