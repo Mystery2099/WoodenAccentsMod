295d7cc5577a8189686c61214e47032a04897207
Mystery2099 <xxmysteryxxftb@gmail.com>
2023-09-02 17:45:53 -0400

    Made the list of entries for a custom item group a list of ItemStack instead of ItemConvertable and made the extra stack with nbt be added to the list

--- src/main/kotlin/com/mystery2099/wooden_accents_mod/item_group/CustomItemGroup.kt
+++ src/main/kotlin/com/mystery2099/wooden_accents_mod/item_group/CustomItemGroup.kt
@@ -4,28 +4,35 @@ import com.mystery2099.wooden_accents_mod.WoodenAccentsMod.toIdentifier
 import com.mystery2099.wooden_accents_mod.block.ModBlocks
 import com.mystery2099.wooden_accents_mod.block.custom.interfaces.CustomItemGroupProvider
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup
-import net.minecraft.item.ItemConvertible
 import net.minecraft.item.ItemGroup
 import net.minecraft.item.ItemStack
 import net.minecraft.item.Items
 
 class CustomItemGroup(name: String) {
     val itemGroup: ItemGroup
-    inline val entries: List<ItemConvertible>
+    inline val entries: List<ItemStack>
         get() = run {
-            val list = mutableListOf<ItemConvertible>()
+            val list = mutableListOf<ItemStack>()
             ModBlocks.blocks.forEach { block ->
-                if (block is CustomItemGroupProvider && block.itemGroup == this) list += block
-            }
-            return list.ifEmpty { listOf(Items.DIRT) }
-        }
+                if (block is CustomItemGroupProvider) {
+                    if (block.itemGroup == this) {
+                        list += block.asItem().defaultStack
+                        if (block.hasNbtVariant) {
+                            list += block.variantWithNbt
+                        }
+                    }
+                }
 
+            }
+            return list.ifEmpty { listOf(Items.DIRT.defaultStack) }
+        }
     init {
         itemGroup = FabricItemGroup.builder(name.toIdentifier()).apply {
-            icon { ItemStack(entries[0]) }
+            icon { entries[0] }
         }.build()
         mutableInstances += this
     }
+
     fun get() = itemGroup
 
     infix operator fun contains(stack: ItemStack) = itemGroup.contains(stack)
