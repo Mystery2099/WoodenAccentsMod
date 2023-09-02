f22019570e543470b185397d7da93ca7e1ee3846
Mystery2099 <xxmysteryxxftb@gmail.com>
2023-09-02 19:19:10 -0400

    Added tagFromProvider variable which checks if the block from a CustomBlockItem is an instance of CustomTagProvider and gets the tag

--- src/main/kotlin/com/mystery2099/wooden_accents_mod/item/CustomBlockItem.kt
+++ src/main/kotlin/com/mystery2099/wooden_accents_mod/item/CustomBlockItem.kt
@@ -1,6 +1,7 @@
 package com.mystery2099.wooden_accents_mod.item
 
 import com.mystery2099.wooden_accents_mod.block.custom.CrateBlock
+import com.mystery2099.wooden_accents_mod.block.custom.interfaces.CustomTagProvider
 import net.minecraft.block.Block
 import net.minecraft.entity.ItemEntity
 import net.minecraft.item.BlockItem
@@ -8,9 +9,16 @@ import net.minecraft.item.ItemStack
 import net.minecraft.item.ItemUsage
 import net.minecraft.nbt.NbtCompound
 import net.minecraft.nbt.NbtElement
+import net.minecraft.registry.tag.TagKey
 
 class CustomBlockItem(block: Block, settings: Settings) : BlockItem(block, settings) {
     override fun canBeNested(): Boolean = block !is CrateBlock
+    val tagFromProvider: TagKey<Block>?
+        get() {
+            return if (block is CustomTagProvider) {
+                (block as CustomTagProvider).tag
+            } else null
+        }
 
     override fun onItemEntityDestroyed(entity: ItemEntity) {
         super.onItemEntityDestroyed(entity)
