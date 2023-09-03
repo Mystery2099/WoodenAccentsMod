0bc591065ea29e48caabed59a33a6cd6a7763748
Mystery2099 <xxmysteryxxftb@gmail.com>
2023-09-03 17:09:50 -0400

    made AbstractDeskBlock use DeskShape instead of StairShape

--- src/main/java/com/mystery2099/wooden_accents_mod/block/custom/AbstractDeskBlock.java
+++ src/main/java/com/mystery2099/wooden_accents_mod/block/custom/AbstractDeskBlock.java
@@ -1,20 +1,20 @@
 package com.mystery2099.wooden_accents_mod.block.custom;
 
+import com.mystery2099.wooden_accents_mod.block.custom.enums.DeskShape;
 import com.mystery2099.wooden_accents_mod.block.custom.interfaces.CustomBlockStateProvider;
 import com.mystery2099.wooden_accents_mod.block.custom.interfaces.CustomItemGroupProvider;
 import com.mystery2099.wooden_accents_mod.block.custom.interfaces.CustomRecipeProvider;
 import com.mystery2099.wooden_accents_mod.block.custom.interfaces.CustomTagProvider;
+import com.mystery2099.wooden_accents_mod.state.property.ModProperties;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.HorizontalFacingBlock;
-import net.minecraft.block.enums.StairShape;
 import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.DirectionProperty;
 import net.minecraft.state.property.EnumProperty;
-import net.minecraft.state.property.Properties;
 import net.minecraft.util.BlockMirror;
 import net.minecraft.util.BlockRotation;
 import net.minecraft.util.math.BlockPos;
@@ -28,7 +28,7 @@ import java.util.Objects;
 
 public abstract class AbstractDeskBlock extends AbstractWaterloggableBlock implements
         CustomItemGroupProvider, CustomTagProvider, CustomRecipeProvider, CustomBlockStateProvider {
-    public static final EnumProperty<StairShape> SHAPE = Properties.STAIR_SHAPE;
+    public static final EnumProperty<DeskShape> SHAPE = ModProperties.getDeskShape();
     public static final DirectionProperty FACING = HorizontalFacingBlock.FACING;
     final Block topBlock, baseBlock;
 
@@ -36,17 +36,17 @@ public abstract class AbstractDeskBlock extends AbstractWaterloggableBlock imple
         super(settings);
         this.topBlock = topBlock;
         this.baseBlock = baseBlock;
-        this.setDefaultState(this.stateManager.getDefaultState().with(FACING, Direction.NORTH).with(SHAPE, StairShape.STRAIGHT));
+        this.setDefaultState(this.stateManager.getDefaultState().with(FACING, Direction.NORTH).with(SHAPE, DeskShape.STRAIGHT));
     }
 
-    private StairShape getDeskShape(BlockState state, BlockView world, BlockPos pos) {
+    private DeskShape getDeskShape(BlockState state, BlockView world, BlockPos pos) {
         Direction direction2;
         var direction = state.get(FACING);
         var blockState2 = world.getBlockState(pos.offset(direction));
         if (isDesk(blockState2) && (direction2 = blockState2.get(FACING)).getAxis() != state.get(FACING).getAxis() && isDifferentOrientation(state, world, pos, direction2.getOpposite())) {
-            return (direction2 == direction.rotateYCounterclockwise()) ? StairShape.INNER_LEFT : StairShape.INNER_RIGHT;
+            return (direction2 == direction.rotateYCounterclockwise()) ? DeskShape.LEFT : DeskShape.RIGHT;
         }
-        return StairShape.STRAIGHT;
+        return DeskShape.STRAIGHT;
     }
 
     public boolean isDesk(BlockState state) {
@@ -100,16 +100,16 @@ public abstract class AbstractDeskBlock extends AbstractWaterloggableBlock imple
             case LEFT_RIGHT -> {
                 if (direction.getAxis() != Direction.Axis.Z) break;
                 return switch (stairShape) {
-                    case INNER_LEFT -> state.rotate(BlockRotation.CLOCKWISE_180).with(SHAPE, StairShape.INNER_RIGHT);
-                    case INNER_RIGHT -> state.rotate(BlockRotation.CLOCKWISE_180).with(SHAPE, StairShape.INNER_LEFT);
+                    case LEFT -> state.rotate(BlockRotation.CLOCKWISE_180).with(SHAPE, DeskShape.RIGHT);
+                    case RIGHT -> state.rotate(BlockRotation.CLOCKWISE_180).with(SHAPE, DeskShape.LEFT);
                     default -> state.rotate(BlockRotation.CLOCKWISE_180);
                 };
             }
             case FRONT_BACK -> {
                 if (direction.getAxis() != Direction.Axis.X) break;
                 return switch (stairShape) {
-                    case INNER_LEFT -> state.rotate(BlockRotation.CLOCKWISE_180).with(SHAPE, StairShape.INNER_LEFT);
-                    case INNER_RIGHT -> state.rotate(BlockRotation.CLOCKWISE_180).with(SHAPE, StairShape.INNER_RIGHT);
+                    case LEFT -> state.rotate(BlockRotation.CLOCKWISE_180).with(SHAPE, DeskShape.LEFT);
+                    case RIGHT -> state.rotate(BlockRotation.CLOCKWISE_180).with(SHAPE, DeskShape.RIGHT);
                     default -> state.rotate(BlockRotation.CLOCKWISE_180);
                 };
             }
