953e686e59d5ba7bdb85526f871a977cdacd0c30
Mystery2099 <xxmysteryxxftb@gmail.com>
2023-09-03 17:10:14 -0400

    created deskShape EnumProperty

--- src/main/kotlin/com/mystery2099/wooden_accents_mod/state/property/ModProperties.kt
+++ src/main/kotlin/com/mystery2099/wooden_accents_mod/state/property/ModProperties.kt
@@ -2,6 +2,7 @@ package com.mystery2099.wooden_accents_mod.state.property
 
 import com.mystery2099.wooden_accents_mod.block.custom.enums.CoffeeTableType
 import com.mystery2099.wooden_accents_mod.block.custom.enums.ConnectingLadderShape
+import com.mystery2099.wooden_accents_mod.block.custom.enums.DeskShape
 import net.minecraft.state.property.BooleanProperty
 import net.minecraft.state.property.EnumProperty
 
@@ -10,4 +11,6 @@ object ModProperties {
     val connectingLadderShape: EnumProperty<ConnectingLadderShape> = EnumProperty.of("shape", ConnectingLadderShape::class.java)
     val left: BooleanProperty = BooleanProperty.of("left")
     val right: BooleanProperty = BooleanProperty.of("right")
+    @JvmStatic
+    val deskShape: EnumProperty<DeskShape> = EnumProperty.of("shape", DeskShape::class.java)
 }
