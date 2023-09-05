7602b0cd0fb9f6f52d7f8222d6c53b72f4c2a930
Mystery2099 <xxmysteryxxftb@gmail.com>
2023-09-05 13:15:43 -0400

    Replaced old functions and parameters with new Updated ones for rotated VoxelShapes and removed CompositeVoxelShape.kt

--- src/main/kotlin/com/mystery2099/wooden_accents_mod/util/VoxelShapeHelper.kt
+++ src/main/kotlin/com/mystery2099/wooden_accents_mod/util/VoxelShapeHelper.kt
@@ -1,7 +1,7 @@
 package com.mystery2099.wooden_accents_mod.util
 
-import com.mystery2099.wooden_accents_mod.WoodenAccentsMod
 import net.minecraft.block.Block
+import net.minecraft.util.function.BooleanBiFunction
 import net.minecraft.util.math.Direction
 import net.minecraft.util.shape.VoxelShape
 import net.minecraft.util.shape.VoxelShapes
@@ -12,10 +12,6 @@ import kotlin.math.min
 object VoxelShapeHelper {
     private inline val Double.limited
         get() = this.limit()
-    inline val Collection<VoxelShape>.combined
-        get() = this.unifyElements()
-    inline val Array<VoxelShape>.combined
-        get() = this.unifyElements()
 
     private inline val VoxelShape.minX
         get() = getMin(Direction.Axis.X)
@@ -30,19 +26,9 @@ object VoxelShapeHelper {
     private inline val VoxelShape.maxZ
         get() = getMax(Direction.Axis.Z)
 
-    fun Collection<VoxelShape>.unifyElements(): VoxelShape = fold(VoxelShapes.empty(), VoxelShapes::union)
-    fun Array<VoxelShape>.unifyElements(): VoxelShape = fold(VoxelShapes.empty(), VoxelShapes::union)
-
     infix fun VoxelShape.unifiedWith(otherShape: VoxelShape): VoxelShape = VoxelShapes.union(this, otherShape)
-    fun VoxelShape.unifiedWith(vararg otherShapes: VoxelShape): VoxelShape = otherShapes.fold(this, VoxelShapes::union)
-    infix fun VoxelShape.unifiedWith(otherShapes: Collection<VoxelShape>): VoxelShape = this.unifiedWith(otherShapes.combined)
-    infix operator fun VoxelShape.plus(otherShape: VoxelShape): VoxelShape = this.unifiedWith(otherShape)
-    infix operator fun VoxelShape.plus(otherShapes: Collection<VoxelShape>): VoxelShape = this.unifiedWith(otherShapes.combined)
-    infix operator fun VoxelShape.plus(otherShapes: Array<VoxelShape>): VoxelShape = this.unifiedWith(otherShapes.combined)
-    infix fun VoxelShape.and(otherShape: VoxelShape): VoxelShape = this.unifiedWith(otherShape)
-    infix fun VoxelShape.and(otherShapes: Array<VoxelShape>): VoxelShape = this.unifiedWith(otherShapes.combined)
-    infix fun VoxelShape.and(otherShapes: Collection<VoxelShape>): VoxelShape = this.unifiedWith(otherShapes.combined)
-
+    fun VoxelShape.unifiedWith(vararg otherShapes: VoxelShape): VoxelShape = union(this, *otherShapes)
+    infix operator fun VoxelShape.plus(otherShape: VoxelShape): VoxelShape = VoxelShapes.union(this, otherShape)
     fun setMaxHeight(source: VoxelShape, height: Double): VoxelShape {
         val result = AtomicReference(VoxelShapes.empty())
         source.forEachBox { minX: Double, minY: Double, minZ: Double, maxX: Double, _: Double, maxZ: Double ->
@@ -61,81 +47,81 @@ object VoxelShapeHelper {
         return result.get()
     }
 
-    //Do not use on complex VoxelShapes
-    infix fun VoxelShape.rotate(direction: VoxelShapeTransformation): VoxelShape = adjustValues(
-        direction, this.minX, this.minZ, this.maxX, this.maxZ
-    ).let { adjustedValues ->
-        VoxelShapes.cuboid(
-            adjustedValues[0], this.minY,
-            adjustedValues[1], adjustedValues[2], this.maxY, adjustedValues[3]
-        )
-    }
-    /*fun VoxelShape.rotate(direction: VoxelShapeRotations): VoxelShape {
-        val adjustedValues = adjustValues(
-            direction, this.getMin(Direction.Axis.X), this.getMin(Direction.Axis.Z), this.getMax(
-                Direction.Axis.X
-            ), this.getMax(Direction.Axis.Z)
-        )
+    /*//Do not use on complex VoxelShapes
+    fun rotate(voxelShape: VoxelShape, direction: VoxelShapeTransformation): VoxelShape {
+        val adjustedValues = adjustValues(direction, voxelShape.minX, voxelShape.minZ, voxelShape.maxX, voxelShape.maxZ)
         return VoxelShapes.cuboid(
-            adjustedValues[0], this.getMin(Direction.Axis.Y),
-            adjustedValues[1], adjustedValues[2], this.getMax(Direction.Axis.Y), adjustedValues[3]
+            adjustedValues[0], voxelShape.minY, adjustedValues[1],
+            adjustedValues[2], voxelShape.maxY, adjustedValues[3]
         )
     }*/
 
-    infix fun MutableCollection<VoxelShape>.rotate(direction: VoxelShapeTransformation) = rotateElements(direction).combined
-    infix fun Array<VoxelShape>.rotate(direction: VoxelShapeTransformation) = rotateElements(direction).combined
-
-
-    fun VoxelShape.rotateLeft() = rotate(VoxelShapeTransformation.ROTATE_LEFT)//Do not use on complex VoxelShapes
-
-    fun MutableCollection<VoxelShape>.rotateLeft() = rotate(VoxelShapeTransformation.ROTATE_LEFT)
-    fun Array<VoxelShape>.rotateLeft() = rotate(VoxelShapeTransformation.ROTATE_LEFT)
-
-
-    fun VoxelShape.rotateRight() = rotate(VoxelShapeTransformation.ROTATE_RIGHT)//Do not use on complex VoxelShapes
-
-    fun MutableCollection<VoxelShape>.rotateRight() = rotate(VoxelShapeTransformation.ROTATE_RIGHT)
-    fun Array<VoxelShape>.rotateRight() = rotate(VoxelShapeTransformation.ROTATE_RIGHT)
-
-
-    fun VoxelShape.flip() = rotate(VoxelShapeTransformation.FLIP) //Do not use on complex VoxelShapes
-
-    fun MutableCollection<VoxelShape>.flip() = rotate(VoxelShapeTransformation.FLIP)
-    fun Array<VoxelShape>.flip() = rotate(VoxelShapeTransformation.FLIP)
-
-    infix fun <t : Collection<VoxelShape>> t.rotateElements(direction: VoxelShapeTransformation): Collection<VoxelShape> {
-        if (isEmpty()) {
-            val callerStackTrace = Thread.currentThread().stackTrace[2]
-            val callerClassName = callerStackTrace.className
-            val callerLineNumber = callerStackTrace.lineNumber
-            WoodenAccentsMod.logger.info("Warning: Collection of VoxelShapes is empty in class: $callerClassName, line: $callerLineNumber. Returning a list containing an empty VoxelShape.")
-            return listOf(VoxelShapes.empty())
+    val VoxelShape.rotatedLeft: VoxelShape
+        get() = this.rotated(VoxelShapeTransformation.ROTATE_LEFT)
+    val VoxelShape.flipped: VoxelShape
+        get() = this.rotated(VoxelShapeTransformation.FLIP_HORIZONTAL)
+    val VoxelShape.rotatedRight: VoxelShape
+        get() = this.rotated(VoxelShapeTransformation.ROTATE_RIGHT)
+    fun VoxelShape.rotated(direction: VoxelShapeTransformation): VoxelShape {
+        val shapes = mutableListOf(VoxelShapes.empty())
+        this.forEachBox { minX, minY, minZ, maxX, maxY, maxZ ->
+            shapes += voxelShapeFromAdjustedValues(
+                direction = direction,
+                minX = minX,
+                minY = minY,
+                minZ = minZ,
+                maxX = maxX,
+                maxY = maxY,
+                maxZ = maxZ
+            )
         }
-        return this.map { it.rotate(direction) }
-    }
-    infix fun Array<VoxelShape>.rotateElements(direction: VoxelShapeTransformation): Array<VoxelShape> {
-        if (isEmpty()) {
-            val callerStackTrace = Thread.currentThread().stackTrace[2]
-            val callerClassName = callerStackTrace.className
-            val callerLineNumber = callerStackTrace.lineNumber
-            WoodenAccentsMod.logger.info("Warning: Array of VoxelShapes is empty in class: $callerClassName, line: $callerLineNumber. Returning an Array containing an empty VoxelShape.")
-            return arrayOf(VoxelShapes.empty())
-        }
-        return map { it.rotate(direction) }.toTypedArray()
+        return shapes.reduce { a, b -> VoxelShapes.union(a, b) }
     }
 
+    fun combine(function: BooleanBiFunction, vararg voxelShapes: VoxelShape): VoxelShape {
+        return voxelShapes.reduce { a, b -> VoxelShapes.combine(a, b, function) }
+    }
 
-    private fun adjustValues(
+    fun union(vararg voxelShapes: VoxelShape): VoxelShape = voxelShapes.reduce { a, b -> VoxelShapes.union(a, b) }
+
+    //Adjuts values depending on the transformation, then returns a new VoxelShape from those values
+    private fun voxelShapeFromAdjustedValues(
         direction: VoxelShapeTransformation,
-        var1: Double,
-        var2: Double,
-        var3: Double,
-        var4: Double
+        minX: Double,
+        minY: Double,
+        minZ: Double,
+        maxX: Double,
+        maxY: Double,
+        maxZ: Double
     ) = when (direction) {
-        VoxelShapeTransformation.FLIP -> doubleArrayOf(1.0f - var3, 1.0f - var4, 1.0f - var1, 1.0f - var2)
-        VoxelShapeTransformation.ROTATE_RIGHT -> doubleArrayOf(var2, 1.0f - var3, var4, 1.0f - var1)
-        VoxelShapeTransformation.ROTATE_LEFT -> doubleArrayOf(1.0f - var4, var1, 1.0f - var2, var3)
-        else -> doubleArrayOf(var1, var2, var3, var4)
+        VoxelShapeTransformation.FLIP_HORIZONTAL -> VoxelShapes.cuboid(
+            /* minX = */ 1.0f - maxX,
+            /* minY = */ minY,
+            /* minZ = */ 1.0f - maxZ,
+            /* maxX = */ 1.0f - minX,
+            /* maxY = */ maxY,
+            /* maxZ = */ 1.0f - minZ
+        )
+
+        VoxelShapeTransformation.ROTATE_RIGHT -> VoxelShapes.cuboid(
+            /* minX = */ minZ,
+            /* minY = */ minY,
+            /* minZ = */ 1.0f - maxX,
+            /* maxX = */ maxZ,
+            /* maxY = */ maxY,
+            /* maxZ = */ 1.0f - minX
+        )
+
+        VoxelShapeTransformation.ROTATE_LEFT -> VoxelShapes.cuboid(
+            /* minX = */ 1.0f - maxZ,
+            /* minY = */ minY,
+            /* minZ = */ minX,
+            /* maxX = */ 1.0f - minZ,
+            /* maxY = */ maxZ,
+            /* maxZ = */ maxX
+        )
+
+        else -> VoxelShapes.cuboid(minX, minY, minZ, maxX, maxY, maxZ)
     }
 
     private fun Double.limit() = max(0.0, min(1.0, this))
@@ -155,6 +141,7 @@ object VoxelShapeHelper {
         maxZ.toDouble()
     )
 }
+
 enum class VoxelShapeTransformation {
-    ROTATE_LEFT, ROTATE_RIGHT, FLIP, ROTATE_UP, ROTATE_DOWN
+    ROTATE_LEFT, ROTATE_RIGHT, FLIP_HORIZONTAL, ROTATE_UP, ROTATE_DOWN
 }
