d39ad43ba46a454912f77ed3a98ed5e727918190
Mystery2099 <xxmysteryxxftb@gmail.com>
2023-09-07 09:46:16 -0400

    overrided functions that will be needed for DeskDrawer in the future and added BlockState properties for it

--- src/main/kotlin/com/mystery2099/wooden_accents_mod/block/custom/DeskDrawerBlock.kt
+++ src/main/kotlin/com/mystery2099/wooden_accents_mod/block/custom/DeskDrawerBlock.kt
@@ -7,23 +7,89 @@ import com.mystery2099.wooden_accents_mod.block.custom.interfaces.CustomTagProvi
 import com.mystery2099.wooden_accents_mod.data.ModBlockTags
 import com.mystery2099.wooden_accents_mod.item_group.CustomItemGroup
 import com.mystery2099.wooden_accents_mod.item_group.ModItemGroups
+import com.mystery2099.wooden_accents_mod.state.property.ModProperties
 import net.minecraft.block.Block
+import net.minecraft.block.BlockRenderType
 import net.minecraft.block.BlockState
+import net.minecraft.block.ShapeContext
 import net.minecraft.block.entity.BlockEntity
 import net.minecraft.data.client.BlockStateModelGenerator
 import net.minecraft.data.server.recipe.RecipeJsonProvider
+import net.minecraft.entity.LivingEntity
+import net.minecraft.item.ItemPlacementContext
+import net.minecraft.item.ItemStack
 import net.minecraft.registry.tag.TagKey
+import net.minecraft.state.StateManager
+import net.minecraft.state.property.BooleanProperty
+import net.minecraft.state.property.DirectionProperty
+import net.minecraft.state.property.Properties
+import net.minecraft.util.BlockMirror
+import net.minecraft.util.BlockRotation
 import net.minecraft.util.math.BlockPos
+import net.minecraft.util.math.Direction
+import net.minecraft.util.shape.VoxelShape
+import net.minecraft.world.BlockView
+import net.minecraft.world.World
 import java.util.function.Consumer
 
 class DeskDrawerBlock(settings: Settings, val baseBlock: Block, val topBlock: Block) :
     WaterloggableBlockWithEntity(settings), CustomItemGroupProvider, CustomRecipeProvider, CustomTagProvider<Block>,
     CustomBlockStateProvider {
 
-    override val itemGroup: CustomItemGroup
-        get() = ModItemGroups.decorations
-    override val tag: TagKey<Block>
-        get() = ModBlockTags.deskDrawers
+    override val itemGroup: CustomItemGroup = ModItemGroups.decorations
+    override val tag: TagKey<Block> = ModBlockTags.deskDrawers
+
+    init {
+        this.defaultState =
+            this.stateManager.defaultState.with(facing, Direction.NORTH).with(open, false).with(left, false)
+                .with(right, false)
+    }
+
+    override fun appendProperties(builder: StateManager.Builder<Block, BlockState>) {
+        super.appendProperties(builder)
+        builder.add(facing, open, left, right)
+    }
+
+    override fun getPlacementState(ctx: ItemPlacementContext): BlockState {
+        return super.getPlacementState(ctx)
+    }
+
+    override fun onPlaced(
+        world: World?,
+        pos: BlockPos?,
+        state: BlockState?,
+        placer: LivingEntity?,
+        itemStack: ItemStack?
+    ) {
+        super.onPlaced(world, pos, state, placer, itemStack)
+    }
+
+    override fun getOutlineShape(
+        state: BlockState?,
+        world: BlockView?,
+        pos: BlockPos?,
+        context: ShapeContext?
+    ): VoxelShape {
+        return super.getOutlineShape(state, world, pos, context)
+    }
+
+    override fun getRenderType(state: BlockState?): BlockRenderType = BlockRenderType.MODEL
+
+    override fun hasComparatorOutput(state: BlockState?): Boolean {
+        return super.hasComparatorOutput(state)
+    }
+
+    override fun getComparatorOutput(state: BlockState?, world: World?, pos: BlockPos?): Int {
+        return super.getComparatorOutput(state, world, pos)
+    }
+
+    override fun rotate(state: BlockState?, rotation: BlockRotation?): BlockState {
+        return super.rotate(state, rotation)
+    }
+
+    override fun mirror(state: BlockState, mirror: BlockMirror): BlockState {
+        return super.mirror(state, mirror)
+    }
 
     override fun createBlockEntity(pos: BlockPos, state: BlockState): BlockEntity {
         TODO("Not yet implemented")
@@ -35,5 +101,12 @@ class DeskDrawerBlock(settings: Settings, val baseBlock: Block, val topBlock: Bl
 
     override fun offerRecipeTo(exporter: Consumer<RecipeJsonProvider>) {
 
+    }
+
+    companion object {
+        val facing: DirectionProperty = Properties.HORIZONTAL_FACING
+        val open: BooleanProperty = Properties.OPEN
+        val left: BooleanProperty = ModProperties.left
+        val right: BooleanProperty = ModProperties.right
     }
 }
