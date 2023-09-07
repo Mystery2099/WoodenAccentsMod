a58b283a59dea0448c31c33531c34361fccad33c
Mystery2099 <xxmysteryxxftb@gmail.com>
2023-09-07 12:18:56 -0400

    Added most functionality to DeskDrawerBlock and block entity

--- src/main/kotlin/com/mystery2099/wooden_accents_mod/block/custom/DeskDrawerBlock.kt
+++ src/main/kotlin/com/mystery2099/wooden_accents_mod/block/custom/DeskDrawerBlock.kt
@@ -1,13 +1,17 @@
 package com.mystery2099.wooden_accents_mod.block.custom
 
+import com.mystery2099.wooden_accents_mod.block.custom.enums.SidewaysConnectionShape
 import com.mystery2099.wooden_accents_mod.block.custom.interfaces.CustomBlockStateProvider
 import com.mystery2099.wooden_accents_mod.block.custom.interfaces.CustomItemGroupProvider
 import com.mystery2099.wooden_accents_mod.block.custom.interfaces.CustomRecipeProvider
 import com.mystery2099.wooden_accents_mod.block.custom.interfaces.CustomTagProvider
+import com.mystery2099.wooden_accents_mod.block_entity.custom.DeskDrawerBlockEntity
 import com.mystery2099.wooden_accents_mod.data.ModBlockTags
+import com.mystery2099.wooden_accents_mod.data.ModBlockTags.isIn
 import com.mystery2099.wooden_accents_mod.item_group.CustomItemGroup
 import com.mystery2099.wooden_accents_mod.item_group.ModItemGroups
 import com.mystery2099.wooden_accents_mod.state.property.ModProperties
+import net.fabricmc.fabric.api.`object`.builder.v1.block.entity.FabricBlockEntityTypeBuilder
 import net.minecraft.block.Block
 import net.minecraft.block.BlockRenderType
 import net.minecraft.block.BlockState
@@ -16,20 +20,27 @@ import net.minecraft.block.entity.BlockEntity
 import net.minecraft.data.client.BlockStateModelGenerator
 import net.minecraft.data.server.recipe.RecipeJsonProvider
 import net.minecraft.entity.LivingEntity
+import net.minecraft.entity.mob.PiglinBrain
+import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.item.ItemPlacementContext
 import net.minecraft.item.ItemStack
 import net.minecraft.registry.tag.TagKey
+import net.minecraft.screen.ScreenHandler
 import net.minecraft.state.StateManager
-import net.minecraft.state.property.BooleanProperty
 import net.minecraft.state.property.DirectionProperty
+import net.minecraft.state.property.EnumProperty
 import net.minecraft.state.property.Properties
+import net.minecraft.util.ActionResult
 import net.minecraft.util.BlockMirror
 import net.minecraft.util.BlockRotation
+import net.minecraft.util.Hand
+import net.minecraft.util.hit.BlockHitResult
 import net.minecraft.util.math.BlockPos
 import net.minecraft.util.math.Direction
 import net.minecraft.util.shape.VoxelShape
 import net.minecraft.world.BlockView
 import net.minecraft.world.World
+import net.minecraft.world.WorldAccess
 import java.util.function.Consumer
 
 class DeskDrawerBlock(settings: Settings, val baseBlock: Block, val topBlock: Block) :
@@ -41,29 +52,85 @@ class DeskDrawerBlock(settings: Settings, val baseBlock: Block, val topBlock: Bl
 
     init {
         this.defaultState =
-            this.stateManager.defaultState.with(facing, Direction.NORTH).with(open, false).with(left, false)
-                .with(right, false)
+            this.stateManager.defaultState.with(facing, Direction.NORTH).withShape(left = false, right = false)
+        blockEntityTypeBuilder.addBlock(this)
+
     }
 
+    private fun BlockState.withShape(left: Boolean, right: Boolean): BlockState = this.withIfExists(
+        shape, when {
+            left && right -> SidewaysConnectionShape.CENTER
+            left -> SidewaysConnectionShape.RIGHT
+            right -> SidewaysConnectionShape.LEFT
+            else -> SidewaysConnectionShape.SINGLE
+        }
+    )
+    private fun BlockState.isDeskDrawer(): Boolean = this isIn tag
+
+    private fun BlockState.canConnectTo(other: BlockState): Boolean {
+        return this.isDeskDrawer() && other.isDeskDrawer() && this[facing] == other[facing]
+    }
     override fun appendProperties(builder: StateManager.Builder<Block, BlockState>) {
         super.appendProperties(builder)
-        builder.add(facing, open, left, right)
+        builder.add(facing, shape)
     }
 
     override fun getPlacementState(ctx: ItemPlacementContext): BlockState {
-        return super.getPlacementState(ctx)
+        return super.getPlacementState(ctx).with(facing, ctx.horizontalPlayerFacing.opposite).withShape(
+            left = false,
+            right = false
+        )
     }
 
     override fun onPlaced(
-        world: World?,
-        pos: BlockPos?,
-        state: BlockState?,
+        world: World,
+        pos: BlockPos,
+        state: BlockState,
         placer: LivingEntity?,
-        itemStack: ItemStack?
+        itemStack: ItemStack
     ) {
-        super.onPlaced(world, pos, state, placer, itemStack)
+        val blockEntity: BlockEntity? = world.getBlockEntity(pos)
+        if (itemStack.hasCustomName() && blockEntity is DeskDrawerBlockEntity) {
+            blockEntity.customName = itemStack.name
+        }
     }
 
+    @Deprecated("Deprecated in Java")
+    override fun onUse(
+        state: BlockState,
+        world: World,
+        pos: BlockPos,
+        player: PlayerEntity,
+        hand: Hand?,
+        hit: BlockHitResult?
+    ): ActionResult {
+        if (world.isClient) return ActionResult.SUCCESS
+        val blockEntity = world.getBlockEntity(pos)
+        if (blockEntity is DeskDrawerBlockEntity) {
+            player.openHandledScreen(blockEntity)
+            PiglinBrain.onGuardedBlockInteracted(player, true)
+        }
+        return ActionResult.CONSUME
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun getStateForNeighborUpdate(
+        state: BlockState,
+        direction: Direction?,
+        neighborState: BlockState?,
+        world: WorldAccess,
+        pos: BlockPos,
+        neighborPos: BlockPos?
+    ): BlockState {
+        val leftState = world.getBlockState(pos.offset(state[facing].rotateYClockwise()))
+        val rightState = world.getBlockState(pos.offset(state[facing].rotateYCounterclockwise()))
+        return super.getStateForNeighborUpdate(state, direction, neighborState, world, pos, neighborPos)
+            .withShape(
+                left = state.canConnectTo(leftState),
+                right = state.canConnectTo(rightState)
+            )
+    }
+    @Deprecated("Deprecated in Java")
     override fun getOutlineShape(
         state: BlockState?,
         world: BlockView?,
@@ -73,26 +140,29 @@ class DeskDrawerBlock(settings: Settings, val baseBlock: Block, val topBlock: Bl
         return super.getOutlineShape(state, world, pos, context)
     }
 
+    @Deprecated("Deprecated in Java")
     override fun getRenderType(state: BlockState?): BlockRenderType = BlockRenderType.MODEL
 
-    override fun hasComparatorOutput(state: BlockState?): Boolean {
-        return super.hasComparatorOutput(state)
+    @Deprecated("Deprecated in Java")
+    override fun hasComparatorOutput(state: BlockState) = true
+
+    @Deprecated("Deprecated in Java")
+    override fun getComparatorOutput(state: BlockState, world: World, pos: BlockPos): Int {
+        return ScreenHandler.calculateComparatorOutput(world.getBlockEntity(pos))
     }
 
-    override fun getComparatorOutput(state: BlockState?, world: World?, pos: BlockPos?): Int {
-        return super.getComparatorOutput(state, world, pos)
+    @Deprecated("Deprecated in Java")
+    override fun rotate(state: BlockState, rotation: BlockRotation): BlockState {
+        return state.with(facing, rotation.rotate(state[facing]))
     }
 
-    override fun rotate(state: BlockState?, rotation: BlockRotation?): BlockState {
-        return super.rotate(state, rotation)
-    }
-
+    @Deprecated("Deprecated in Java")
     override fun mirror(state: BlockState, mirror: BlockMirror): BlockState {
-        return super.mirror(state, mirror)
+        return state.rotate(mirror.getRotation(state[facing]))
     }
 
     override fun createBlockEntity(pos: BlockPos, state: BlockState): BlockEntity {
-        TODO("Not yet implemented")
+        return DeskDrawerBlockEntity(pos, state)
     }
 
     override fun generateBlockStateModels(generator: BlockStateModelGenerator) {
@@ -105,8 +175,8 @@ class DeskDrawerBlock(settings: Settings, val baseBlock: Block, val topBlock: Bl
 
     companion object {
         val facing: DirectionProperty = Properties.HORIZONTAL_FACING
-        val open: BooleanProperty = Properties.OPEN
-        val left: BooleanProperty = ModProperties.left
-        val right: BooleanProperty = ModProperties.right
+        val shape: EnumProperty<SidewaysConnectionShape> = ModProperties.sidewaysConnectionShape
+        val blockEntityTypeBuilder: FabricBlockEntityTypeBuilder<DeskDrawerBlockEntity> =
+            FabricBlockEntityTypeBuilder.create(::DeskDrawerBlockEntity)
     }
 }
---
+++ src/main/kotlin/com/mystery2099/wooden_accents_mod/block_entity/custom/DeskDrawerBlockEntity.kt
@@ -0,0 +1,104 @@
+package com.mystery2099.wooden_accents_mod.block_entity.custom
+
+import com.mystery2099.wooden_accents_mod.block_entity.ModBlockEntities
+import net.minecraft.block.BlockState
+import net.minecraft.block.entity.LootableContainerBlockEntity
+import net.minecraft.entity.Entity
+import net.minecraft.entity.player.PlayerEntity
+import net.minecraft.entity.player.PlayerInventory
+import net.minecraft.inventory.Inventories
+import net.minecraft.item.ItemStack
+import net.minecraft.nbt.NbtCompound
+import net.minecraft.screen.GenericContainerScreenHandler
+import net.minecraft.screen.ScreenHandler
+import net.minecraft.sound.SoundCategory
+import net.minecraft.sound.SoundEvents
+import net.minecraft.text.Text
+import net.minecraft.util.collection.DefaultedList
+import net.minecraft.util.math.BlockPos
+import net.minecraft.world.event.GameEvent
+
+class DeskDrawerBlockEntity(blockPos: BlockPos, blockState: BlockState) :
+    LootableContainerBlockEntity(ModBlockEntities.deskDrawer, blockPos, blockState) {
+    private var inventory = DefaultedList.ofSize(27, ItemStack.EMPTY)
+    private var viewerCount = 0
+    override fun size(): Int = inventory.size
+
+    override fun getContainerName(): Text = Text.translatable(cachedState.block.translationKey)
+
+    override fun createScreenHandler(syncId: Int, playerInventory: PlayerInventory?): ScreenHandler? {
+        return GenericContainerScreenHandler.createGeneric9x3(syncId, playerInventory, this)
+    }
+
+    override fun getInvStackList(): DefaultedList<ItemStack> = inventory
+
+    override fun setInvStackList(list: DefaultedList<ItemStack>) {
+        inventory = list
+    }
+
+    override fun onSyncedBlockEvent(type: Int, data: Int): Boolean {
+        if (type == 1) {
+            viewerCount = data
+            return true
+        }
+        return super.onSyncedBlockEvent(type, data)
+    }
+
+    override fun writeNbt(nbt: NbtCompound?) {
+        super.writeNbt(nbt)
+        if (!this.serializeLootTable(nbt)) {
+            Inventories.writeNbt(nbt, inventory)
+        }
+    }
+
+    override fun readNbt(nbt: NbtCompound?) {
+        super.readNbt(nbt)
+        inventory = DefaultedList.ofSize(this.size(), ItemStack.EMPTY)
+        if (!this.deserializeLootTable(nbt)) {
+            Inventories.readNbt(nbt, inventory)
+        }
+    }
+
+    override fun onOpen(player: PlayerEntity) {
+        if (!removed && !player.isSpectator) {
+            if (viewerCount < 0) {
+                viewerCount = 0
+            }
+            ++viewerCount
+            world?.let { world ->
+                world.addSyncedBlockEvent(pos, cachedState.block, 1, viewerCount)
+                if (viewerCount == 1) {
+                    world.emitGameEvent(player as Entity, GameEvent.CONTAINER_OPEN, pos)
+                    world.playSound(
+                        null,
+                        pos,
+                        SoundEvents.BLOCK_BARREL_OPEN,
+                        SoundCategory.BLOCKS,
+                        0.5f,
+                        world.random.nextFloat() * 0.1f + 0.9f
+                    )
+                }
+            }
+        }
+    }
+
+    override fun onClose(player: PlayerEntity) {
+        if (!removed && !player.isSpectator) {
+            --viewerCount
+            world?.let { world ->
+                world.addSyncedBlockEvent(pos, cachedState.block, 1, viewerCount)
+                if (viewerCount <= 0) {
+                    world.emitGameEvent(player as Entity, GameEvent.CONTAINER_CLOSE, pos)
+                    world.playSound(
+                        null,
+                        pos,
+                        SoundEvents.BLOCK_BARREL_CLOSE,
+                        SoundCategory.BLOCKS,
+                        0.5f,
+                        world.random.nextFloat() * 0.1f + 0.9f
+                    )
+                }
+            }
+        }
+    }
+}
