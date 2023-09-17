8f7285dc43b34f47dc968728b9197181f54c4310
Mystery2099 <xxmysteryxxftb@gmail.com>
2023-09-17 18:55:42 -0400

    Changed how seatEntity is registered

--- src/main/kotlin/com/mystery2099/wooden_accents_mod/entity/ModEntities.kt
+++ src/main/kotlin/com/mystery2099/wooden_accents_mod/entity/ModEntities.kt
@@ -5,14 +5,21 @@ import com.mystery2099.wooden_accents_mod.WoodenAccentsModRegistry
 import com.mystery2099.wooden_accents_mod.entity.custom.SeatEntity
 import net.fabricmc.fabric.api.`object`.builder.v1.entity.FabricEntityTypeBuilder
 import net.minecraft.entity.EntityType
+import net.minecraft.entity.SpawnGroup
 import net.minecraft.registry.Registries
 import net.minecraft.registry.Registry
 
+
 object ModEntities : WoodenAccentsModRegistry {
-    val seatEntity: EntityType<SeatEntity> =
-        Registry.register(Registries.ENTITY_TYPE, "seat".toIdentifier(), FabricEntityTypeBuilder.create<SeatEntity>().build())
+    lateinit var seatEntity: EntityType<SeatEntity>
 
     override fun register() {
+        seatEntity = Registry.register(
+            Registries.ENTITY_TYPE, "seat".toIdentifier(), FabricEntityTypeBuilder.create(
+                SpawnGroup.MISC
+            ) { _, world -> SeatEntity(world) }.build()
+        )
+
         super.register()
     }
 }
