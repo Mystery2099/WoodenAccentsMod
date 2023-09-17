d8c36390c5b3558194d8a8918d143a66d871041c
Mystery2099 <xxmysteryxxftb@gmail.com>
2023-09-17 17:46:22 -0400

    Added code for spawning chair entity and for generating blockstate models

--- src/main/kotlin/com/mystery2099/wooden_accents_mod/block/custom/ChairBlock.kt
+++ src/main/kotlin/com/mystery2099/wooden_accents_mod/block/custom/ChairBlock.kt
@@ -1,19 +1,30 @@
 package com.mystery2099.wooden_accents_mod.block.custom
 
 import com.mystery2099.wooden_accents_mod.data.ModBlockTags
+import com.mystery2099.wooden_accents_mod.data.ModModels
 import com.mystery2099.wooden_accents_mod.data.generation.interfaces.CustomBlockStateProvider
 import com.mystery2099.wooden_accents_mod.data.generation.interfaces.CustomItemGroupProvider
 import com.mystery2099.wooden_accents_mod.data.generation.interfaces.CustomRecipeProvider
 import com.mystery2099.wooden_accents_mod.data.generation.interfaces.CustomTagProvider
+import com.mystery2099.wooden_accents_mod.entity.custom.SeatEntity
 import com.mystery2099.wooden_accents_mod.item_group.CustomItemGroup
 import com.mystery2099.wooden_accents_mod.item_group.ModItemGroups
 import net.fabricmc.fabric.api.`object`.builder.v1.block.FabricBlockSettings
 import net.minecraft.block.Block
+import net.minecraft.block.BlockState
 import net.minecraft.block.HorizontalFacingBlock
 import net.minecraft.block.Waterloggable
 import net.minecraft.data.client.BlockStateModelGenerator
+import net.minecraft.data.client.TextureMap
+import net.minecraft.data.client.TexturedModel
 import net.minecraft.data.server.recipe.RecipeJsonProvider
+import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.registry.tag.TagKey
+import net.minecraft.util.ActionResult
+import net.minecraft.util.Hand
+import net.minecraft.util.hit.BlockHitResult
+import net.minecraft.util.math.BlockPos
+import net.minecraft.world.World
 import java.util.function.Consumer
 
 class ChairBlock(settings: Settings, val baseBlock: Block) : HorizontalFacingBlock(settings), Waterloggable,
@@ -26,10 +37,28 @@ class ChairBlock(settings: Settings, val baseBlock: Block) : HorizontalFacingBlo
     constructor(baseBlock: Block) : this(FabricBlockSettings.copyOf(baseBlock), baseBlock)
 
     override fun generateBlockStateModels(generator: BlockStateModelGenerator) {
-
+        val texturedModel = TexturedModel.makeFactory({ TextureMap.all(baseBlock) }, ModModels.basicChair)
+        generator.registerNorthDefaultHorizontalRotated(this, texturedModel)
     }
 
     override fun offerRecipeTo(exporter: Consumer<RecipeJsonProvider>) {
 
+    }
+
+    override fun onUse(
+        state: BlockState,
+        world: World,
+        pos: BlockPos?,
+        player: PlayerEntity,
+        hand: Hand?,
+        hit: BlockHitResult?
+    ): ActionResult {
+        if (world.isClient()) {
+            return ActionResult.SUCCESS;
+        }
+        val seat = SeatEntity(world)
+        world.spawnEntity(seat)
+        player.startRiding(seat)
+        return ActionResult.SUCCESS
     }
 }
