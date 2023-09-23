3087a1678f76e45d10311be433a565019e645112
Mystery2099 <xxmysteryxxftb@gmail.com>
2023-09-23 19:29:01 -0400

    chair blocks are now waterloggable

--- src/main/kotlin/com/mystery2099/wooden_accents_mod/block/custom/ChairBlock.kt
+++ src/main/kotlin/com/mystery2099/wooden_accents_mod/block/custom/ChairBlock.kt
@@ -10,6 +10,7 @@ import com.mystery2099.wooden_accents_mod.entity.ModEntities
 import com.mystery2099.wooden_accents_mod.entity.custom.SeatEntity
 import com.mystery2099.wooden_accents_mod.item_group.CustomItemGroup
 import com.mystery2099.wooden_accents_mod.item_group.ModItemGroups
+import com.mystery2099.wooden_accents_mod.util.BlockStateUtil.withProperties
 import com.mystery2099.wooden_accents_mod.util.VoxelShapeHelper
 import com.mystery2099.wooden_accents_mod.util.VoxelShapeHelper.flipped
 import com.mystery2099.wooden_accents_mod.util.VoxelShapeHelper.rotatedLeft
@@ -21,9 +22,13 @@ import net.minecraft.data.client.TextureMap
 import net.minecraft.data.client.TexturedModel
 import net.minecraft.data.server.recipe.RecipeJsonProvider
 import net.minecraft.entity.player.PlayerEntity
+import net.minecraft.fluid.FluidState
+import net.minecraft.fluid.Fluids
 import net.minecraft.item.ItemPlacementContext
 import net.minecraft.registry.tag.TagKey
 import net.minecraft.state.StateManager
+import net.minecraft.state.property.BooleanProperty
+import net.minecraft.state.property.Properties
 import net.minecraft.util.ActionResult
 import net.minecraft.util.Hand
 import net.minecraft.util.hit.BlockHitResult
@@ -33,6 +38,7 @@ import net.minecraft.util.shape.VoxelShape
 import net.minecraft.util.shape.VoxelShapes
 import net.minecraft.world.BlockView
 import net.minecraft.world.World
+import net.minecraft.world.WorldAccess
 import java.util.function.Consumer
 
 
@@ -44,19 +50,24 @@ class ChairBlock(settings: Settings, val baseBlock: Block) : HorizontalFacingBlo
     override val tag: TagKey<Block> = ModBlockTags.chairs
 
     init {
-        this.defaultState = defaultState.with(FACING, Direction.NORTH)
+        this.defaultState = defaultState.withProperties {
+            waterlogged setTo false
+            FACING setTo Direction.NORTH
+        }
     }
 
     constructor(baseBlock: Block) : this(FabricBlockSettings.copyOf(baseBlock), baseBlock)
 
     override fun appendProperties(builder: StateManager.Builder<Block, BlockState>) {
         super.appendProperties(builder)
-        builder.add(FACING)
+        builder.add(FACING, waterlogged)
     }
 
-    override fun getPlacementState(ctx: ItemPlacementContext): BlockState? {
-        return defaultState.with(FACING, ctx.horizontalPlayerFacing.opposite)
-
+    override fun getPlacementState(ctx: ItemPlacementContext): BlockState {
+        return defaultState.withProperties {
+            waterlogged setTo ctx.world.getFluidState(ctx.blockPos).isOf(Fluids.WATER)
+            FACING setTo ctx.horizontalPlayerFacing.opposite
+        }
     }
 
     @Deprecated("Deprecated in Java")
@@ -94,6 +105,35 @@ class ChairBlock(settings: Settings, val baseBlock: Block) : HorizontalFacingBlo
         Direction.WEST -> westShape
         else -> VoxelShapes.fullCube()
     }
+
+    @Deprecated("Deprecated in Java")
+    override fun getStateForNeighborUpdate(
+        state: BlockState,
+        direction: Direction?,
+        neighborState: BlockState?,
+        world: WorldAccess,
+        pos: BlockPos,
+        neighborPos: BlockPos?
+    ): BlockState = state.also {
+        if (it[waterlogged]) world.scheduleFluidTick(
+            pos,
+            Fluids.WATER,
+            Fluids.WATER.getTickRate(world)
+        )
+    }
+
+    @Deprecated("Deprecated in Java", ReplaceWith(
+        "if (state[waterlogged]) Fluids.WATER.getStill(false) else super.getFluidState(state)",
+        "com.mystery2099.wooden_accents_mod.block.custom.ChairBlock.Companion.waterlogged",
+        "net.minecraft.fluid.Fluids",
+        "net.minecraft.block.HorizontalFacingBlock"
+    )
+    )
+    override fun getFluidState(state: BlockState): FluidState {
+        return if (state[waterlogged]) Fluids.WATER.getStill(false)
+        else super.getFluidState(state)
+    }
+
     override fun generateBlockStateModels(generator: BlockStateModelGenerator) {
         val texturedModel = TexturedModel.makeFactory({ TextureMap.all(baseBlock) }, ModModels.basicChair)
         generator.registerNorthDefaultHorizontalRotated(this, texturedModel)
@@ -102,7 +142,9 @@ class ChairBlock(settings: Settings, val baseBlock: Block) : HorizontalFacingBlo
     override fun offerRecipeTo(exporter: Consumer<RecipeJsonProvider>) {
 
     }
+
     companion object {
+        val waterlogged: BooleanProperty = Properties.WATERLOGGED
         private val northShape = VoxelShapeHelper.union(
             VoxelShapeHelper.createCuboidShape(2, 0, 2, 4, 8, 4),
             VoxelShapeHelper.createCuboidShape(12, 0, 2, 14, 8, 4),
@@ -112,8 +154,8 @@ class ChairBlock(settings: Settings, val baseBlock: Block) : HorizontalFacingBlo
             VoxelShapeHelper.createCuboidShape(2, 16, 12, 14, 20, 14),
             VoxelShapeHelper.createCuboidShape(2, 8, 2, 14, 10, 14)
         )
-        val eastShape = northShape.rotatedLeft
-        val southShape = northShape.flipped
-        val westShape = northShape.rotatedRight
+        private val eastShape = northShape.rotatedLeft
+        private val southShape = northShape.flipped
+        private val westShape = northShape.rotatedRight
     }
 }
