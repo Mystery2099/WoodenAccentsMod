2118fb0be270dbe27e0dc278387a9f043a1ae045
Mystery2099 <xxmysteryxxftb@gmail.com>
2023-09-23 18:07:23 -0400

    created chair outline shapes

--- src/main/kotlin/com/mystery2099/wooden_accents_mod/block/custom/ChairBlock.kt
+++ src/main/kotlin/com/mystery2099/wooden_accents_mod/block/custom/ChairBlock.kt
@@ -10,11 +10,12 @@ import com.mystery2099.wooden_accents_mod.entity.ModEntities
 import com.mystery2099.wooden_accents_mod.entity.custom.SeatEntity
 import com.mystery2099.wooden_accents_mod.item_group.CustomItemGroup
 import com.mystery2099.wooden_accents_mod.item_group.ModItemGroups
+import com.mystery2099.wooden_accents_mod.util.VoxelShapeHelper
+import com.mystery2099.wooden_accents_mod.util.VoxelShapeHelper.flipped
+import com.mystery2099.wooden_accents_mod.util.VoxelShapeHelper.rotatedLeft
+import com.mystery2099.wooden_accents_mod.util.VoxelShapeHelper.rotatedRight
 import net.fabricmc.fabric.api.`object`.builder.v1.block.FabricBlockSettings
-import net.minecraft.block.Block
-import net.minecraft.block.BlockState
-import net.minecraft.block.HorizontalFacingBlock
-import net.minecraft.block.Waterloggable
+import net.minecraft.block.*
 import net.minecraft.data.client.BlockStateModelGenerator
 import net.minecraft.data.client.TextureMap
 import net.minecraft.data.client.TexturedModel
@@ -28,6 +29,9 @@ import net.minecraft.util.Hand
 import net.minecraft.util.hit.BlockHitResult
 import net.minecraft.util.math.BlockPos
 import net.minecraft.util.math.Direction
+import net.minecraft.util.shape.VoxelShape
+import net.minecraft.util.shape.VoxelShapes
+import net.minecraft.world.BlockView
 import net.minecraft.world.World
 import java.util.function.Consumer
 
@@ -52,15 +56,6 @@ class ChairBlock(settings: Settings, val baseBlock: Block) : HorizontalFacingBlo
 
     override fun getPlacementState(ctx: ItemPlacementContext): BlockState? {
         return defaultState.with(FACING, ctx.horizontalPlayerFacing.opposite)
-
-    }
-
-    override fun generateBlockStateModels(generator: BlockStateModelGenerator) {
-        val texturedModel = TexturedModel.makeFactory({ TextureMap.all(baseBlock) }, ModModels.basicChair)
-        generator.registerNorthDefaultHorizontalRotated(this, texturedModel)
-    }
-
-    override fun offerRecipeTo(exporter: Consumer<RecipeJsonProvider>) {
 
     }
 
@@ -91,4 +86,39 @@ class ChairBlock(settings: Settings, val baseBlock: Block) : HorizontalFacingBlo
         }
     }
 
+    @Deprecated("Deprecated in Java")
+    override fun getOutlineShape(
+        state: BlockState,
+        world: BlockView?,
+        pos: BlockPos?,
+        context: ShapeContext?
+    ): VoxelShape = when (state[FACING]) {
+        Direction.NORTH -> northShape
+        Direction.EAST -> eastShape
+        Direction.SOUTH -> southShape
+        Direction.WEST -> westShape
+        else -> VoxelShapes.fullCube()
+    }
+    override fun generateBlockStateModels(generator: BlockStateModelGenerator) {
+        val texturedModel = TexturedModel.makeFactory({ TextureMap.all(baseBlock) }, ModModels.basicChair)
+        generator.registerNorthDefaultHorizontalRotated(this, texturedModel)
+    }
+
+    override fun offerRecipeTo(exporter: Consumer<RecipeJsonProvider>) {
+
+    }
+    companion object {
+        private val northShape = VoxelShapeHelper.union(
+            VoxelShapeHelper.createCuboidShape(2, 0, 2, 4, 8, 4),
+            VoxelShapeHelper.createCuboidShape(12, 0, 2, 14, 8, 4),
+            VoxelShapeHelper.createCuboidShape(12, 0, 12, 14, 8, 14),
+            VoxelShapeHelper.createCuboidShape(2, 0, 12, 4, 8, 14),
+            VoxelShapeHelper.createCuboidShape(2, 10, 12, 14, 16, 14),
+            VoxelShapeHelper.createCuboidShape(2, 16, 12, 14, 20, 14),
+            VoxelShapeHelper.createCuboidShape(2, 8, 2, 14, 10, 14)
+        )
+        val eastShape = northShape.rotatedLeft
+        val southShape = northShape.flipped
+        val westShape = northShape.rotatedRight
+    }
 }
