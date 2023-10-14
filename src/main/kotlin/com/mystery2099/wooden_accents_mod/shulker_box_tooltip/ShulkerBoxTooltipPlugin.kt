f063421dffebca95d5b148284bc0072d5c4340a1
Mystery2099 <xxmysteryxxftb@gmail.com>
2023-10-14 18:33:18 -0400

    Now using filterIsInstance method instead of a switch so each id is only register once

--- src/main/kotlin/com/mystery2099/wooden_accents_mod/shulker_box_tooltip/ShulkerBoxTooltipPlugin.kt
+++ src/main/kotlin/com/mystery2099/wooden_accents_mod/shulker_box_tooltip/ShulkerBoxTooltipPlugin.kt
@@ -8,35 +8,24 @@ import com.mystery2099.wooden_accents_mod.block.ModBlocks
 import com.mystery2099.wooden_accents_mod.block.custom.CrateBlock
 import com.mystery2099.wooden_accents_mod.block.custom.KitchenCabinetBlock
 import com.mystery2099.wooden_accents_mod.block.custom.ThinBookshelfBlock
+import net.minecraft.block.Block
 
 object ShulkerBoxTooltipPlugin : ShulkerBoxTooltipApi {
     override fun registerProviders(registry: PreviewProviderRegistry) {
-        ModBlocks.blocks.forEach { block ->
-            when (block) {
-                is CrateBlock -> {
-                    registry.register(
-                        "crates".toIdentifier(),
-                        CratePreviewProvider(),
-                        block.asItem()
-                    )
-                }
-
-                is KitchenCabinetBlock -> {
-                    registry.register(
-                        "chest_like".toIdentifier(),
-                        BlockEntityPreviewProvider(27, true),
-                        block.asItem()
-                    )
-                }
-
-                is ThinBookshelfBlock -> {
-                    registry.register(
-                        "chiseled_bookshelf_like".toIdentifier(),
-                        BlockEntityPreviewProvider(6, true, 3),
-                        block.asItem()
-                    )
-                }
-            }
-        }
+        registry.register(
+            "crates".toIdentifier(),
+            CratePreviewProvider(),
+            *(ModBlocks.blocks.filterIsInstance<CrateBlock>().map(Block::asItem).toTypedArray())
+        )
+        registry.register(
+            "chest_like".toIdentifier(),
+            BlockEntityPreviewProvider(27, true),
+            *(ModBlocks.blocks.filterIsInstance<KitchenCabinetBlock>().map(Block::asItem).toTypedArray())
+        )
+        registry.register(
+            "chiseled_bookshelf_like".toIdentifier(),
+            BlockEntityPreviewProvider(6, true, 3),
+            *(ModBlocks.blocks.filterIsInstance<ThinBookshelfBlock>().map(Block::asItem).toTypedArray())
+        )
     }
 }
