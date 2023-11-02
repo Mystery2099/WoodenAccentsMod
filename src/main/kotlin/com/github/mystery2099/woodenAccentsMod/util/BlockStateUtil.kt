df17d7564f7b1d92103821792dbef62da31446e1
Mathew Kennedy-Brewer <xxmysteryxxftb@gmail.com>
2023-11-02 12:22:15 -0400

    Added some documentation

--- src/main/kotlin/com/github/mystery2099/woodenAccentsMod/util/BlockStateUtil.kt
+++ src/main/kotlin/com/github/mystery2099/woodenAccentsMod/util/BlockStateUtil.kt
@@ -1,66 +1,90 @@
 package com.github.mystery2099.woodenAccentsMod.util
 
+import com.github.mystery2099.woodenAccentsMod.util.BlockStateUtil.withProperties
+import net.minecraft.block.AbstractBlock.AbstractBlockState
 import net.minecraft.block.Block
 import net.minecraft.block.BlockState
+import net.minecraft.fluid.Fluid
+import net.minecraft.fluid.FluidState
 import net.minecraft.registry.tag.TagKey
 import net.minecraft.state.property.Property
 
+/**
+ * A utility object providing additional functions to be used with [BlockState]
+ */
 object BlockStateUtil {
     /**
-     * With properties
+     * Stores a copy of the [BlockState] it is applied to inside an instance of [BlockStateConfigurer].
      *
-     * @param configure
-     * @receiver
-     * @return the configured block state
+     * This allows you to set the properties of the stored [BlockState] in a more readable way and returns the newly modified [BlockState].
+     *
+     * @param configure The functions to be applied to the [BlockState] after storing it within a [BlockStateConfigurer] instance.
+     * @receiver [BlockState]
+     * @return The configured [BlockState].
+     *
+     * @see BlockStateConfigurer
+     * @see BlockStateConfigurer.setTo
+     * @see BlockStateConfigurer.set
      */
     @JvmStatic
-    infix fun BlockState.withProperties(configure: BlockStateConfigurer.() -> Unit): BlockState {
+    inline fun BlockState.withProperties(configure: BlockStateConfigurer.() -> Unit): BlockState {
         val builder = BlockStateConfigurer(this)
         builder.configure()
         return builder.blockState
     }
 
-    infix fun BlockState?.isOf(block: Block): Boolean = this?.isOf(block) ?: false
-    infix fun BlockState?.isIn(tag: TagKey<Block>?): Boolean = this?.isIn(tag) ?: false
+    /**
+     * Checks if an instance of [AbstractBlockState] is of a specific [Block].
+     * An infix and null-checking version of [AbstractBlockState.isOf].
+     *
+     * @param block The [Block] instance to check against.
+     * @return `true` if the [BlockState] is of the [block], `false` otherwise.
+     *
+     * @see AbstractBlockState.isOf
+     */
+    infix fun AbstractBlockState?.isOf(block: Block): Boolean = this?.isOf(block) ?: false
+
+    infix fun AbstractBlockState?.isIn(tag: TagKey<Block>?): Boolean = this?.isIn(tag) ?: false
+
+    infix fun FluidState?.isOf(fluid: Fluid): Boolean = this?.isOf(fluid) ?: false
 
 }
 
 /**
- * Block state configurer
+ * Stores a [BlockState] instance and provides methods for adding properties to it in a more readable way than using [BlockState.with] for each property.
  *
- * @property blockState
- * @constructor Create Block state configurer containing a block state
+ * @property blockState The [BlockState] to be stored and modified in this class's instance.
+ * @constructor Creates a Block state configurer containing a [BlockState].
+ *
+ * @see BlockStateUtil.withProperties
  */
 class BlockStateConfigurer(var blockState: BlockState) {
     /**
-     * Set to
+     * Sets the value of a [Property] to a [value] in the [blockState]. This is the infix version of [set].
      *
-     * @param T
-     * @param value
+     * @param T The type of property to set in the [blockState].
+     * @param value The value to set to the [Property] of [T].
+     *
+     * @see set
      */
+
     infix fun <T : Comparable<T>> Property<T>.setTo(value: T) {
         set(this, value)
     }
 
     /**
-     * Set
+     * Sets the value of a [property] to a [value] in the [blockState].
      *
-     * @param T
-     * @param property
-     * @param value
+     * @param T The type of [Property] to set in the [blockState].
+     * @param property The [Property] to set in the [blockState].
+     * @param value The value to set to the [Property] of [T].
+     *
+     * @see BlockState.with
+     * @see setTo
      */
-    operator fun <T : Comparable<T>> set(property: Property<T>, value: T) {
+    fun <T : Comparable<T>> set(property: Property<T>, value: T) {
         blockState = blockState.with(property, value)
     }
-
-    /**
-     * Get
-     *
-     * @param T
-     * @param property
-     * @return
-     */
-    operator fun <T : Comparable<T>> get(property: Property<T>): T = blockState[property]
 }
 
 
