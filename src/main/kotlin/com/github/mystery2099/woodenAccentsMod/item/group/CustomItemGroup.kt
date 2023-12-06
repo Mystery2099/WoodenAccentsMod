a768112bf1b836650c16a2989d1444e1ca1abd04
Mathew Kennedy-Brewer <xxmysteryxxftb@gmail.com>
2023-12-06 14:44:20 -0500

    Refactor and expand CustomItemGroup.kt
    The CustomItemGroup.kt file is heavily refactored for clarity and functionality. Code was added to manage item stack variants and handle cases where the item group lists are empty, defaulting to a DIRT item stack. Furthermore, detailed documentation comments for most methods were included for easier understanding of the functionality.

--- src/main/kotlin/com/github/mystery2099/woodenAccentsMod/item/group/CustomItemGroup.kt
+++ src/main/kotlin/com/github/mystery2099/woodenAccentsMod/item/group/CustomItemGroup.kt
@@ -1,72 +1,136 @@
 package com.github.mystery2099.woodenAccentsMod.item.group
 
 import com.github.mystery2099.woodenAccentsMod.WoodenAccentsMod.toIdentifier
+import com.github.mystery2099.woodenAccentsMod.block.ModBlocks
 import com.github.mystery2099.woodenAccentsMod.block.defaultItemStack
 import com.github.mystery2099.woodenAccentsMod.data.generation.interfaces.CustomItemGroupProvider
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup
 import net.minecraft.block.Block
-import net.minecraft.item.*
+import net.minecraft.item.BlockItem
+import net.minecraft.item.ItemGroup
+import net.minecraft.item.ItemStack
+import net.minecraft.item.Items
 
 /**
- * [CustomItemGroup] is a data class representing a custom [ItemGroup] for organizing and displaying [Item]s in the creative menu.
+ * Represents a custom item group for organizing related items.
  *
- * @constructor Create a custom [ItemGroup] with the given name.
- * @param name The name of the custom [ItemGroup].
- * @see ModItemGroups
- * @see ItemGroup
- * @see ItemGroups
+ * @property name The name of the custom item group.
+ * @property itemGroup The associated [ItemGroup] for this custom item group.
+ * @property entries A list of item stack entries for this item group.
  */
 data class CustomItemGroup(val name: String) {
-    // The ItemGroup associated with this custom item group.
+    init {
+        mutableInstances += this
+    }
+    /**
+     * Represents an item group for a set of related items.
+     *
+     * @property itemGroup The associated ItemGroup for this item group.
+     */
     val itemGroup: ItemGroup = FabricItemGroup.builder(name.toIdentifier()).apply {
         icon { entries[0] }
     }.build()
 
-    // Get a list of item stack entries for this item group.
-    inline val entries: List<ItemStack>
-        get() {
-            val list = mutableListOf<ItemStack>()
-            val altList = mutableListOf<ItemStack>()
 
-            com.github.mystery2099.woodenAccentsMod.block.ModBlocks.blocks.filterIsInstance<CustomItemGroupProvider>()
-                .filter { it.itemGroup == this }
-                .forEach {
-                    list += (it as? Block)?.defaultItemStack ?: ItemStack.EMPTY
-                    if (it.hasVariantItemGroupStack) {
-                        altList += it.variantItemGroupStack
-                    }
-                }
+    /**
+     * Retrieves a list of [ItemStack] entries for the CustomItemGroup.
+     *
+     * This method retrieves a list of [ItemStack] entries for the CustomItemGroup. It first calls the
+     * [getBlocksWithMatchingItemGroup] function to obtain a list of blocks that have a matching itemGroup.
+     * Then, it calls the [getStackListWithVariants] function to extract the itemStack entries from
+     * the list of CustomItemGroupProvider objects. Finally, it calls the [addDefaultStackIfEmpty] function
+     * to add a default DIRT item stack to the list if it is empty.
+     *
+     * @return The list of [ItemStack] entries for the CustomItemGroup.
+     */
+    val entries: List<ItemStack>
+        get() = getEntries()
+    /**
+     * Retrieves a list of [ItemStack] entries for the CustomItemGroup.
+     *
+     * This method retrieves a list of [ItemStack] entries for the CustomItemGroup. It first calls the
+     * getBlocksWithMatchingItemGroup() function to obtain a list of blocks that have a matching itemGroup.
+     * Then, it calls the getStackListWithVariants() function to extract the itemStack entries from
+     * the list of CustomItemGroupProvider objects. Finally, it calls the [addDefaultStackIfEmpty] function
+     * to add a default DIRT item stack to the list if it is empty.
+     *
+     * @return The list of [ItemStack] entries for the CustomItemGroup.
+     */
+    private fun getEntries(): List<ItemStack> {
+        val matchingItems = getBlocksWithMatchingItemGroup()
+        val stacksList = getStackListWithVariants(matchingItems)
 
-            altList.forEach { alt ->
-                val element = list.lastOrNull {
-                    (it.item as BlockItem).block.javaClass == (alt.item as BlockItem).block.javaClass
-                    //(it.item as CustomBlockItem).tagFromProvider == (alt.item as CustomBlockItem).tagFromProvider
-                }
-                list.indexOf(element).let {
-                    if (it > -1) list.add(it + 1, alt)
-                }
-            }
-
-            return list.ifEmpty { list + Items.DIRT.defaultStack }
-        }
-
-    init {
-        mutableInstances += this
+        return addDefaultStackIfEmpty(stacksList)
     }
 
     /**
-     * Get the associated [ItemGroup].
+     * Retrieves a list of [ItemStack] entries for a specific CustomItemGroup that have variants.
      *
-     * @return The [ItemGroup] associated with this [CustomItemGroup].
+     * This method takes a list of [CustomItemGroupProvider] objects and extracts the [ItemStack] entries from them.
+     * For each provider in the list, the method checks if it has a variant item stack. If it does, the variant
+     * item stack is added to the list. The method also adds the default item stack for each provider that
+     * implements the [Block] interface. Finally, the method rearranges the list by moving the variant item stacks
+     * next to their corresponding default item stacks, where the order is determined by the order of appearance in the
+     * original list.
+     *
+     * @param matchingItems The list of [CustomItemGroupProvider] objects to extract the [ItemStack] entries from.
+     * @return The list of [ItemStack] entries for the CustomItemGroup with variants.
+     */
+    private fun getStackListWithVariants(matchingItems: List<CustomItemGroupProvider>): MutableList<ItemStack> {
+        val list = mutableListOf<ItemStack>()
+        val altList = mutableListOf<ItemStack>()
+
+        matchingItems.forEach {
+            list += (it as? Block)?.defaultItemStack ?: ItemStack.EMPTY
+            if (it.hasVariantItemGroupStack) {
+                altList += it.variantItemGroupStack
+            }
+        }
+
+        altList.forEach { alt ->
+            val element = list.lastOrNull {
+                (it.item as BlockItem).block.javaClass == (alt.item as BlockItem).block.javaClass
+                //(it.item as CustomBlockItem).tagFromProvider == (alt.item as CustomBlockItem).tagFromProvider
+            }
+            list.indexOf(element).let {
+                if (it > -1) list.add(it + 1, alt)
+            }
+        }
+        return list
+    }
+
+    /**
+     * Adds a default [ItemStack] for the DIRT item to the list if it is empty.
+     *
+     * @param list The list of [ItemStack] to be checked and modified.
+     * @return The modified list of [ItemStack] with the default DIRT item added if it was empty.
+     */
+    private fun addDefaultStackIfEmpty(list: MutableList<ItemStack>): List<ItemStack> {
+        return list.ifEmpty { list + Items.DIRT.defaultStack }
+    }
+
+    /**
+     * Retrieves a list of blocks with a matching item group.
+     *
+     * This method filters the blocks in the `ModBlocks` registry that implement the `CustomItemGroupProvider` interface
+     * and have a matching itemGroup. The filtered blocks are then returned as a list.
+     *
+     * @return The list of blocks with a matching item group.
+     */
+    private fun getBlocksWithMatchingItemGroup() = ModBlocks.blocks.filterIsInstance<CustomItemGroupProvider>()
+        .filter { it.itemGroup == this }
+
+
+
+
+    /**
+     * Retrieves the item group associated with the CustomItemGroupProvider.
+     *
+     * @return The CustomItemGroup associated with the provider.
      */
     fun get() = itemGroup
 
-    /**
-     * Check if a given [ItemStack] is contained in [itemGroup].
-     *
-     * @param stack The [ItemStack] to check.
-     * @return true if the [ItemStack] is contained, false otherwise.
-     */
+
     infix operator fun contains(stack: ItemStack) = itemGroup.contains(stack)
 
     companion object {
